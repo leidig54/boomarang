e20f2f7f0c43cef48003f375f1088952d3afd4d4
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-06 14:52:57 +0000

    feat(initialization): initialize Flutter project with Firebase and cloud functions
    - add initial Flutter project structure
    - configure Firebase options
    - create basic user authentication with Firebase Auth
    - setup Firestore and Cloud Functions integration
    - add initial UI elements including navigation and report generation features
    - include macOS support and configuration files
    - add web support with manifest and index.html
    - create .gitignore files for various platforms
    - replace default app icons and add splash screens

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,80 +3,40 @@ import { configureGenkit } from "@genkit-ai/core";
 import { firebase } from "@genkit-ai/firebase";
 import { firebaseAuth } from "@genkit-ai/firebase/auth";
 import { onFlow } from "@genkit-ai/firebase/functions";
-import { gemini15ProPreview, vertexAI } from "@genkit-ai/vertexai";
+import { geminiPro, googleAI } from "@genkit-ai/googleai";
 import * as z from "zod";
-import serviceAccount from "./serviceKey.json";
-import admin = require("firebase-admin");
-
-
-admin.initializeApp(
-  {
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    storageBucket: "boomarang-ac130.appspot.com",
-  }
-);
 
 configureGenkit({
-  plugins: [
-    firebase(),
-    vertexAI({ location: "europe-west2" }),
-  ],
+  plugins: [firebase(), googleAI()],
   logLevel: "debug",
   enableTracingAndMetrics: true,
 });
 
-
-export const extractQuillDeltaFromFile = onFlow(
+export const writeReportFlow = onFlow(
   {
-    name: "extractQuillDeltaFromFile",
-    inputSchema: z.object({
-      fileId: z.string(),
-      userId: z.string(),
-    }),
+    name: "writeReportFlow",
+    inputSchema: z.string(),
     outputSchema: z.string(),
     authPolicy: firebaseAuth((user) => {
-      if (user.uid === null) {
-        throw new Error("Verified email required to run flow");
+      if (!user) {
+        throw new Error("User must be signed in to run flow");
       }
     }),
   },
   async (subject) => {
-    console.log(subject);
-    const fileId = subject.fileId;
-    const userId = subject.userId;
-
-    // get file url from storage. the ref is the user id and the file id separated by a slash
-    const bucket = admin.storage().bucket();
-    const fileRef = bucket.file(`${userId}/${fileId}`);
-
-    // get signed url
-    const [signedUrl] = await fileRef.getSignedUrl({
-      action: "read",
-      expires: Date.now() + 1000 * 60 * 5,
-    });
-
-    console.log(signedUrl);
-
-    // get file content type
-    const [metadata] = await fileRef.getMetadata();
-    const fileContentType = metadata.contentType;
-
-
-    const result = await generate({
-      model: gemini15ProPreview,
-      prompt: [
-        // eslint-disable-next-line max-len
-        { text: "Extract the text from this pdf and give it to me in a quill delta format (json)for use in my app. Do not include ```json etc, its gets plugged straight into a jsonDecoder: No need for the ops stuff either. Always end in a newline. Add some heading formatting etc so it looks nice." },
-        { media: { url: signedUrl, contentType: fileContentType } },
-      ],
+    const prompt = `You are an medical admin assisstant you need to write a medical report to satisfy
+    the following request, given the following information: ${subject}.
+     Only use what is containined in the information provided. Do not add any extra findings,
+     recommendations or anything else that is not explicitly stated in the information provided.`;
+    const llmResponse = await generate({
+      model: geminiPro,
+      prompt: prompt,
       config: {
-        temperature: 0.5,
+        temperature: 1,
       },
     });
 
-    console.log(result.text());
-
-    return result.text();
+    return llmResponse.text();
   }
 );
 
