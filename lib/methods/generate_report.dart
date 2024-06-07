25d7e86201a6288e01c08f95440a67b93738c58f
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-07 11:53:35 +0000

    fix(generate_report): Handle upload errors by rethrowing exceptions
    - Added error rethrowing in the generateReport method to ensure proper error handling.
    - Updated pubspec.yaml and pubspec.lock to remove unused dependencies.

--- lib/methods/generate_report.dart
+++ lib/methods/generate_report.dart
@@ -29,6 +29,7 @@ Future<Document> generateReport({
             ))
         .catchError((error) {
       debugPrint('Error uploading file: $error');
+      throw error;
     }).then((value) {
       debugPrint('File uploaded successfully');
     });
@@ -49,6 +50,7 @@ Future<Document> generateReport({
             ))
         .catchError((error) {
       debugPrint('Error uploading file: $error');
+      throw error;
     }).then((value) {
       debugPrint('File uploaded successfully');
     });
