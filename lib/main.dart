73e852a8bfb7ef74dbaa62e6ad7f4978f655b9df
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-07 11:53:35 +0000

    refactor(generate_report): Improve error handling in report generation
    - Added error rethrowing in the file upload process to ensure errors bubble up properly.
    - Removed unused dependencies from pubspec.lock and pubspec.yaml, including convert, petitparser, syncfusion_flutter_core, syncfusion_flutter_pdf, and xml.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:boomarang/firebase_options.dart';
-import 'package:boomarang/screens/add_request.dart';
+import 'package:boomarang/screens/add_request/shell.dart';
 import 'package:boomarang/screens/inbox.dart';
 import 'package:boomarang/screens/profile.dart';
-import 'package:boomarang/screens/sent.dart';
+import 'package:boomarang/screens/sandbox.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -10,7 +10,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFunctions functions =
@@ -38,14 +37,6 @@ class _MainAppState extends State<MainApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', 'GB'), // English, UK
-      ],
       routes: {
         '/': (context) => const InitApp(),
         '/add-request': (context) => const AddRequestScreen(),
@@ -160,8 +151,8 @@ class _HomeState extends State<Home> {
 
   List<Widget> screens = [
     const InboxScreen(),
-    const SentScreen(),
     const ProfileScreen(),
+    const SandboxScreen(),
     const Text('Settings'),
   ];
 
@@ -179,15 +170,13 @@ class _HomeState extends State<Home> {
               icon: Icon(Icons.mail),
               label: Text('Inbox'),
             ),
-            //Sent
-            NavigationRailDestination(
-              icon: Icon(Icons.send),
-              label: Text('Sent'),
-            ),
-
             NavigationRailDestination(
               icon: Icon(Icons.account_circle),
-              label: Text('Profile'),
+              label: Text('Account'),
+            ),
+            NavigationRailDestination(
+              icon: Icon(Icons.person),
+              label: Text('Sandbox'),
             ),
             NavigationRailDestination(
               icon: Icon(Icons.settings),
@@ -201,7 +190,7 @@ class _HomeState extends State<Home> {
               selectedIndex = index;
             });
           },
-          extended: MediaQuery.of(context).size.width > 1400,
+          extended: true,
         ),
         const VerticalDivider(
           thickness: 1,
