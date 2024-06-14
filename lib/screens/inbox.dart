190924b4cf8b1fb0857cafb5a75c5317b2ccd9f5
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-14 16:21:26 +0000

    feat(request management): Enhance request handling with new fields and form integrations
    - Refactored request model to include creator ID, organization names, and additional request statuses.
    - Implemented AddRequestScreen for creating and managing requests with multi-step form.
    - Updated InboxScreen to dynamically show requests with an improved data table structure.
    - Improved error handling and user feedback in various screens and form submissions.

--- lib/screens/inbox.dart
+++ lib/screens/inbox.dart
@@ -2,8 +2,7 @@ import 'dart:async';
 
 import 'package:boomarang/main.dart';
 import 'package:boomarang/models/request.dart';
-import 'package:boomarang/screens/add_request.dart';
-import 'package:boomarang/screens/view_request.dart';
+import 'package:boomarang/screens/add_request/shell.dart';
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 
@@ -116,7 +115,7 @@ class _InboxScreenState extends State<InboxScreen> {
               ),
               DataColumn(
                 label: Text("Actions"),
-              ),
+              )
             ],
             rows: _requests
                 .map((request) => DataRow(
@@ -137,26 +136,7 @@ class _InboxScreenState extends State<InboxScreen> {
                             builder: (context) {
                               if (request.isSubmitted == true) {
                                 return TextButton.icon(
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return Dialog(
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(16),
-                                            ),
-                                            clipBehavior: Clip.antiAlias,
-                                            child: SizedBox(
-                                              width: 800,
-                                              height: 800,
-                                              child: ViewRequestScreen(
-                                                request: request,
-                                              ),
-                                            ),
-                                          );
-                                        });
-                                  },
+                                  onPressed: () {},
                                   label: const Text("View"),
                                 );
                               } else {
