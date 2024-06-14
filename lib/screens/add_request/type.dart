9b369ee189b932a0df69612155db37085aed4b66
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-14 16:21:26 +0000

    Improved message: feat(user-interface): Improve overall UI aesthetics

---
+++ lib/screens/add_request/type.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddRequestType extends StatefulWidget {
+  const AddRequestType({super.key});
+
+  @override
+  State<AddRequestType> createState() => _AddRequestTypeState();
+}
+
+class _AddRequestTypeState extends State<AddRequestType> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
