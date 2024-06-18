bd029555f6d9f5ed05967ada2d2504d065bf1d50
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-18 07:55:42 +0000

    feat(add_request): Enhance AddRequest flow with additional information and localization support
    - Refactored AddRequest screen and request model to incorporate authoriser's date of birth and verification details.
    - Introduced ViewRequest screen for viewing request details with proper formating of dates and consent status.
    - Added support for localization in the main app.
    - Updated pubspec.yaml to include necessary packages for form validation and localization.

--- lib/models/request.dart
+++ lib/models/request.dart
@@ -8,6 +8,9 @@ class BoomarangRequest {
   String? authoriserFirstName;
   String? authoriserLastName;
   String? authoriserEmail;
+  bool? authoriserEmailVerified;
+  DateTime? authoriserDOB;
+  bool? authoriserDOBVerified;
   String? requesterUserId;
   String? requesterOrgName;
   String? holderUserId;
@@ -32,6 +35,9 @@ class BoomarangRequest {
     required this.authoriserFirstName,
     required this.authoriserLastName,
     required this.authoriserEmail,
+    required this.authoriserEmailVerified,
+    required this.authoriserDOB,
+    this.authoriserDOBVerified,
     required this.requesterUserId,
     required this.requesterOrgName,
     required this.holderUserId,
@@ -59,6 +65,10 @@ class BoomarangRequest {
       'authoriserFirstName': authoriserFirstName,
       'authoriserLastName': authoriserLastName,
       'authoriserEmail': authoriserEmail,
+      'authoriserEmailVerified': authoriserEmailVerified,
+      'authoriserDOB':
+          authoriserDOB != null ? Timestamp.fromDate(authoriserDOB!) : null,
+      'authoriserDOBVerified': authoriserDOBVerified,
       'requesterUserId': requesterUserId,
       'requesterOrgName': requesterOrgName,
       'holderUserId': holderUserId,
@@ -94,6 +104,15 @@ class BoomarangRequest {
           : null,
       authoriserEmail: map['authoriserEmail'] != null
           ? map['authoriserEmail'] as String
+          : null,
+      authoriserEmailVerified: map['authoriserEmailVerified'] != null
+          ? map['authoriserEmailVerified'] as bool
+          : null,
+      authoriserDOB: map['authoriserDOB'] != null
+          ? ((map['authoriserDOB'] ?? Timestamp(0, 0)) as Timestamp).toDate()
+          : null,
+      authoriserDOBVerified: map['authoriserDOBVerified'] != null
+          ? map['authoriserDOBVerified'] as bool
           : null,
       requesterUserId: map['requesterUserId'] != null
           ? map['requesterUserId'] as String
@@ -143,5 +162,19 @@ class BoomarangRequest {
 
   String get formattedCreatedDate {
     return DateFormat.yMMMMEEEEd().add_jms().format(dateCreated);
+  }
+
+  String get formattedAuthoriserDob {
+    if (authoriserDOB == null) return 'N/A';
+    //dd/MM/yyyy
+    return DateFormat('dd/MM/yyyy').format(authoriserDOB!);
+  }
+
+  String get formattedConsentStatus {
+    //capitalise first letter, remove underscores.
+    return consentStatus!
+        .split('_')
+        .map((word) => word[0].toUpperCase() + word.substring(1))
+        .join(' ');
   }
 }
