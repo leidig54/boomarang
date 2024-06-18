82d135f6171573226a78e3238db7a35b6d857309
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-18 11:23:05 +0000

    Revised message: feat: Add new staff member functionality

--- lib/screens/add_request.dart
+++ lib/screens/add_request.dart
@@ -553,6 +553,7 @@ class _AddRequestScreenState extends State<AddRequestScreen> {
         key: _requestFormKey,
         child: Stepper(
           currentStep: currentStep,
+          type: StepperType.horizontal,
           controlsBuilder: (context, controlsDetails) {
             return Column(
               children: [
@@ -645,8 +646,6 @@ class _AddRequestScreenState extends State<AddRequestScreen> {
   }
 
   Future<void> saveRequest({required bool submit}) async {
-    print(_requestFormKey
-        .currentState!.fields['authoriser_dob']?.value.runtimeType);
     BoomarangRequest newRequest = BoomarangRequest(
       id: id,
       creatorId: request?.creatorId ?? auth.currentUser!.uid,
@@ -673,6 +672,7 @@ class _AddRequestScreenState extends State<AddRequestScreen> {
       consentStatus: hasConsent == true
           ? 'consent_received_from_requester'
           : 'consent_pending',
+      consentVerified: false,
       paymentStatus: 'payment_pending',
       requestStatus: 'request_pending',
       requestType: _requestFormKey.currentState!.fields['type']?.value,
