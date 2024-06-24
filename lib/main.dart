1da9ebf00249f30a515537900be8d6ca4b575440
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-24 13:22:18 +0000

    feat(firebase): Add email notifications for consent requests
    - Implemented Firebase functions to send email notifications on consent request submissions.
    - Added Nodemailer for sending emails.
    - Created 'sendConsentAppWhenRequestSubmitted' function to handle email sending logic based on Firestore triggers.
    - Enhanced the app structure by adding a SentScreen to view all submitted requests.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:boomarang/firebase_options.dart';
 import 'package:boomarang/screens/add_request.dart';
 import 'package:boomarang/screens/inbox.dart';
 import 'package:boomarang/screens/profile.dart';
-import 'package:boomarang/screens/sandbox.dart';
+import 'package:boomarang/screens/sent.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -160,8 +160,8 @@ class _HomeState extends State<Home> {
 
   List<Widget> screens = [
     const InboxScreen(),
+    const SentScreen(),
     const ProfileScreen(),
-    const SandboxScreen(),
     const Text('Settings'),
   ];
 
@@ -179,13 +179,15 @@ class _HomeState extends State<Home> {
               icon: Icon(Icons.mail),
               label: Text('Inbox'),
             ),
+            //Sent
+            NavigationRailDestination(
+              icon: Icon(Icons.send),
+              label: Text('Sent'),
+            ),
+
             NavigationRailDestination(
               icon: Icon(Icons.account_circle),
-              label: Text('Account'),
-            ),
-            NavigationRailDestination(
-              icon: Icon(Icons.person),
-              label: Text('Sandbox'),
+              label: Text('Profile'),
             ),
             NavigationRailDestination(
               icon: Icon(Icons.settings),
@@ -199,7 +201,7 @@ class _HomeState extends State<Home> {
               selectedIndex = index;
             });
           },
-          extended: true,
+          extended: MediaQuery.of(context).size.width > 1400,
         ),
         const VerticalDivider(
           thickness: 1,
