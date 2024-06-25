c4bedab056bb6803752f0b6da1f8790b9824690b
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-25 13:50:39 +0000

    feat(authentication): Add user type handling and dynamic UI updates
    - Added StreamBuilder for user data in AuthGate.
    - Implemented user type differentiation in Home screen to show relevant screens.
    - Enhanced user settings with dropdown for selecting user type.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,322 +1,140 @@
 import 'package:boomarang/main.dart';
-import 'package:boomarang/misc/alert_dialog.dart';
 import 'package:boomarang_shared/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  State<SettingsScreen> createState() => _SettingsScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _SettingsScreenState extends State<SettingsScreen> {
   final _userFormKey = GlobalKey<FormBuilderState>();
-  bool isLoading = false;
-  bool _formChanged = false;
-  bool isSending = false;
-  bool isVerifying = false;
-
-  BoomarangUser? user;
-
-  @override
-  void initState() {
-    getUser();
-    super.initState();
-  }
-
-  Future<void> getUser() async {
-    setState(() {
-      isLoading = true;
-    });
-    await firestore
-        .collection('users')
-        .doc(auth.currentUser!.uid)
-        .get()
-        .then((snapshot) {
-      if (snapshot.exists) {
-        user = BoomarangUser.fromMap(snapshot.data() as Map<String, dynamic>);
-      }
-    }).whenComplete(() {
-      setState(() {
-        isLoading = false;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+    return Scaffold(
+      body: Column(
+        children: [
+          FutureBuilder(
+              future: firestore
+                  .collection('users')
+                  .doc(auth.currentUser!.uid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
 
-    if (user?.emailVerified != true) {
-      if (user?.emailVerificationCodeHasExpired == true) {
-        //resend email button
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-                'Your email has not been verified. Please verify your email to continue.'),
-            const SizedBox(height: 16),
-            isSending
-                ? const LinearProgressIndicator()
-                : TextButton(
-                    onPressed: () async {
-                      setState(() {
-                        isSending = true;
-                      });
-                      //send the user id to the cloud function
-                      await functions
-                          .httpsCallable('sendVerificationEmailCallable')
-                          .call()
-                          .catchError((e) {
-                        setState(() {
-                          isSending = false;
-                        });
-                        buildErrorAlertDialog(e);
-                        throw e;
-                      }).whenComplete(() {
-                        setState(() {
-                          isSending = false;
-                        });
-                        getUser();
-                      });
-                    },
-                    child: const Text('Resend verification email'),
-                  ),
-          ],
-        );
-      } else {
-        return Center(
-          child: SizedBox(
-            width: 400,
-            child: TextField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                labelText: 'Enter the verification code sent to your email',
-                border: OutlineInputBorder(),
-              ),
-              onSubmitted: (code) async {
-                setState(() {
-                  isVerifying = true;
-                });
-                await functions
-                    .httpsCallable('checkEmailVerificationCode')
-                    .call({'code': code}).catchError((e) {
-                  buildErrorAlertDialog(e);
-                  throw e;
-                }).whenComplete(() {
-                  setState(() {
-                    isVerifying = false;
-                  });
-                  getUser();
-                });
-              },
-            ),
-          ),
-        );
-      }
-    }
+                BoomarangUser? user;
 
-    if (user?.userType == null) {
-      //userType (holder or requester)
-      return Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 600),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Select User Type",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 32),
-              ListTile(
-                title: const Text('Holder'),
-                subtitle: const Text(
-                    'You hold sensitive data and want to respond to requests.\nE.g. doctors, accountants, universities.'),
-                trailing: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-                leading: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.arrow_circle_up),
-                  ],
-                ),
-                isThreeLine: true,
-                onTap: () async {
-                  await firestore
-                      .collection('users')
-                      .doc(auth.currentUser!.uid)
-                      .set({'userType': 'holder'}, SetOptions(merge: true));
-                  await getUser();
-                },
-              ),
-              const Divider(
-                thickness: 1,
-                height: 1,
-              ),
-              ListTile(
-                title: const Text('Requester'),
-                subtitle: const Text(
-                    'You want to submit requests for sensitive data.\nE.g. insurance, legal, recruitment.'),
-                trailing: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-                leading: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.arrow_circle_down),
-                  ],
-                ),
-                isThreeLine: true,
-                onTap: () async {
-                  await firestore
-                      .collection('users')
-                      .doc(auth.currentUser!.uid)
-                      .set({'userType': 'requester'}, SetOptions(merge: true));
-                  await getUser();
-                },
-              ),
-            ],
-          ),
-        ),
-      );
-    }
+                if (snapshot.hasData && snapshot.data!.exists) {
+                  user = BoomarangUser.fromMap(
+                      snapshot.data!.data() as Map<String, dynamic>);
+                }
 
-    return FormBuilder(
-      key: _userFormKey,
-      onChanged: () {
-        if (!_formChanged) {
-          setState(() {
-            _formChanged = true;
-          });
-        }
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 600),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Profile',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 32),
-                FormBuilderTextField(
-                  name: 'title',
-                  autofocus: user?.title == null,
-                  validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(),
-                  ]),
-                  initialValue: user?.title,
-                  decoration: const InputDecoration(
-                    labelText: 'Title',
-                    hintText: 'Mr, Mrs, Dr, etc.',
-                    border: UnderlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                FormBuilderTextField(
-                  name: 'firstName',
-                  validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(),
-                    FormBuilderValidators.minLength(2),
-                  ]),
-                  initialValue: user?.firstName,
-                  decoration: const InputDecoration(
-                    labelText: 'First Name',
-                    border: UnderlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                FormBuilderTextField(
-                  name: 'lastName',
-                  validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(),
-                    FormBuilderValidators.minLength(2),
-                  ]),
-                  initialValue: user?.lastName,
-                  decoration: const InputDecoration(
-                    labelText: 'Last Name',
-                    border: UnderlineInputBorder(),
-                  ),
-                ),
-
-                const SizedBox(height: 16),
-                //verify email
-                FormBuilderTextField(
-                  name: 'email',
-                  readOnly: true,
-                  initialValue: user?.email,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: UnderlineInputBorder(),
-                    helperText: 'Email cannot be changed',
-                  ),
-                ),
-                //if email is not verified, show a button to resend verification email or verify email
-                const SizedBox(height: 32),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.save),
-                      onPressed: !_formChanged
-                          ? null
-                          : () async {
-                              if (_userFormKey.currentState!
-                                  .saveAndValidate()) {
-                                final data = _userFormKey.currentState!.value;
-                                await firestore
-                                    .collection('users')
-                                    .doc(auth.currentUser!.uid)
-                                    .set(data, SetOptions(merge: true));
-                                await getUser();
-                                setState(() {
-                                  _formChanged = false;
-                                });
-                              }
-                            },
-                      label: const Text('Save'),
+                return FormBuilder(
+                  key: _userFormKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      constraints: const BoxConstraints(maxWidth: 400),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Profile',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          const SizedBox(height: 16),
+                          FormBuilderTextField(
+                            name: 'title',
+                            initialValue: user?.title,
+                            decoration: const InputDecoration(
+                              labelText: 'Title',
+                              hintText: 'Mr, Mrs, Dr, etc.',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          FormBuilderTextField(
+                            name: 'firstName',
+                            initialValue: user?.firstName,
+                            decoration: const InputDecoration(
+                              labelText: 'First Name',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          FormBuilderTextField(
+                            name: 'lastName',
+                            initialValue: user?.lastName,
+                            decoration: const InputDecoration(
+                              labelText: 'Last Name',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 32),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () async {
+                                  if (_userFormKey.currentState!
+                                      .saveAndValidate()) {
+                                    final data =
+                                        _userFormKey.currentState!.value;
+                                    await firestore
+                                        .collection('users')
+                                        .doc(auth.currentUser!.uid)
+                                        .set(data, SetOptions(merge: true));
+                                    setState(() {});
+                                    showAdaptiveDialog(
+                                        context: navigatorKey.currentContext!,
+                                        builder: (context) {
+                                          return AlertDialog.adaptive(
+                                            title:
+                                                const Text('Profile Updated'),
+                                            content: const Text(
+                                                'Your profile has been updated.'),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                            ],
+                                          );
+                                        });
+                                  }
+                                },
+                                child: const Text('Save'),
+                              ),
+                              const SizedBox(width: 16),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _userFormKey.currentState!.reset();
+                                },
+                                child: const Text('Discard Changes'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    const SizedBox(width: 16),
-                    ElevatedButton.icon(
-                      onPressed: !_formChanged
-                          ? null
-                          : () {
-                              _userFormKey.currentState!.reset();
-                              setState(() {
-                                _formChanged = false;
-                              });
-                            },
-                      icon: const Icon(Icons.clear),
-                      label: const Text('Discard'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
+                  ),
+                );
+              }),
+        ],
       ),
     );
   }
