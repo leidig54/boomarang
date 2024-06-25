9373a0ea4041081923b3236fff1f2a402927016d
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-25 12:11:27 +0000

    refactor(firebase): Refactor Firebase hosting configuration and clean up code
    - Removed unused cache entries in .firebase/hosting.YnVpbGQvd2Vi.cache
    - Updated firebase.json by removing empty 'extensions' field
    - Cleaned up emulator port related code in functions/src/index.ts
    - Removed debugging print statement in lib/screens/inbox.dart

--- lib/screens/inbox.dart
+++ lib/screens/inbox.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:boomarang/main.dart';
-import 'package:boomarang/screens/add_request.dart';
 import 'package:boomarang/screens/view_request.dart';
 import 'package:boomarang_shared/models/request.dart';
 import 'package:boomarang_shared/models/response.dart';
@@ -89,6 +88,7 @@ class _InboxScreenState extends State<InboxScreen> {
           (request) {
             BoomarangResponse? response = _responses
                 .firstWhereOrNull((element) => element.id == request.id);
+
             return DataRow(
               cells: [
                 DataCell(
@@ -97,12 +97,12 @@ class _InboxScreenState extends State<InboxScreen> {
                 DataCell(Text(request.authoriserEmail ?? 'Unknown')),
                 DataCell(Text(request.requestEmail ?? 'Unknown')),
                 DataCell(
-                  Text(response?.status ?? 'Not Started'),
+                  Text(response?.status ?? 'Awaiting response'),
                 ),
                 DataCell(
                   Builder(
                     builder: (context) {
-                      if (request.isSubmitted == true) {
+                      if (response?.isSubmitted != true) {
                         return TextButton.icon(
                           onPressed: () {
                             showDialog(
@@ -126,28 +126,7 @@ class _InboxScreenState extends State<InboxScreen> {
                           label: const Text("Respond"),
                         );
                       } else {
-                        return TextButton.icon(
-                          onPressed: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return Dialog(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16),
-                                    ),
-                                    clipBehavior: Clip.antiAlias,
-                                    child: SizedBox(
-                                      width: 1200,
-                                      height: 800,
-                                      child: AddRequestScreen(
-                                        request: request,
-                                      ),
-                                    ),
-                                  );
-                                });
-                          },
-                          label: const Text("Edit"),
-                        );
+                        return Container();
                       }
                     },
                   ),
