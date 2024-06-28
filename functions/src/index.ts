07a8db9e8a8bc52f0b6a20b451ef479847fba3de
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-28 12:19:26 +0000

    Improved message: feat: Implement basic application functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -92,6 +92,7 @@ async (subject) => {
 export const sendConsentAppWhenRequestSubmitted = functions.firestore.document("requests/{requestId}").onWrite(async (change, context) => {
   // we need to check if to see if the isSubmitted field is true when the request is created or updated (i.e. when the request is submitted), but we only want to send the email once, so we need to check if the isSubmitted field is true and the request has not been submitted before
   if (change.after.data()?.isSubmitted === true && change.before.data()?.isSubmitted !== true) {
+    console.log("Request has been submitted. Sending email to authoriser...");
     const request = change.after.data();
 
     // get the authoriser email from the request
@@ -110,7 +111,7 @@ export const sendConsentAppWhenRequestSubmitted = functions.firestore.document("
       },
     });
 
-    const address = isEmulator ? "http://localhost:54919" : "https://booomarang-consent.web.app";
+    const address = isEmulator ? "http://localhost:62409" : "https://booomarang-consent.web.app";
 
     // the website url is booomarang-consent.web.app. append the request id to the url with the name requestId.
     const emailMessageHtml = `<p>Dear Authoriser,</p>
@@ -135,9 +136,10 @@ export const sendConsentAppWhenRequestSubmitted = functions.firestore.document("
       console.error("There was an error while sending the email:", error);
       return null;
     }
+  } else {
+    console.log("Request has not been submitted. Exiting...");
+    return null;
   }
-
-  return null;
 });
 
 export const createUserDocument = functions.auth.user().onCreate(async (user) => {
@@ -217,6 +219,8 @@ export const assignHolderIdToRequest = functions.firestore.document("requests/{r
           console.error("Error fetching user:", error);
         }
       }
+    } else {
+      console.log("Holder email has not changed. Exiting...");
     }
     return null;
   });
