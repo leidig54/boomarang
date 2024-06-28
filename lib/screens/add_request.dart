c4af7e8374973b8b79b9d9afeffc598cfaa0ac7d
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-28 12:19:26 +0000

    Improved message: feat: Implement new feature for user functionality

--- lib/screens/add_request.dart
+++ lib/screens/add_request.dart
@@ -684,7 +684,7 @@ class _AddRequestScreenState extends State<AddRequestScreen> {
           _requestFormKey.currentState!.fields['authoriser_dob']?.value ?? ""),
       authoriserEmailVerified: false,
       // authoriserPhoneNumber: _requestFormKey.currentState!.fields['authoriser_phone_number']!.value as String,
-      requesterId: auth.currentUser!.uid,
+      requesterUserId: auth.currentUser!.uid,
       requesterOrgName: null,
       requestEmail: auth.currentUser!.email,
       holderUserId: request?.holderUserId,
