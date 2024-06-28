7389ae9382b786ba2cc6ebff696fbb54f1811f34
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-28 08:56:49 +0000

    Refactor: Improve code readability and functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:boomarang/firebase_options.dart';
+import 'package:boomarang/misc/alert_dialog.dart';
 import 'package:boomarang/screens/add_request.dart';
 import 'package:boomarang/screens/inbox.dart';
 import 'package:boomarang/screens/profile.dart';
@@ -184,7 +185,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int selectedIndex = 0;
   String? userType;
+  bool? emailVerified = false;
   bool hasLoaded = false;
+  bool codeExpired = true;
+  DateTime? verificationCodeExpiresAt;
 
   List<Widget> screens = [];
   late StreamSubscription? userTypeStreamSubscription;
@@ -195,7 +199,16 @@ class _HomeState extends State<Home> {
         .doc(auth.currentUser!.uid)
         .snapshots()
         .listen((snapshot) {
-      userType = snapshot.data()!['type'];
+      userType = snapshot.data()?['userType'];
+      emailVerified = snapshot.data()?['emailVerified'];
+
+      verificationCodeExpiresAt =
+          (snapshot.data()?['verificationCodeExpiresAt'] as Timestamp).toDate();
+
+      if (verificationCodeExpiresAt != null) {
+        codeExpired = verificationCodeExpiresAt!.isBefore(DateTime.now());
+      }
+
       if (userType == 'requester') {
         screens = [
           const SentScreen(),
@@ -293,8 +306,112 @@ class _HomeState extends State<Home> {
         ),
         Expanded(
           child: Scaffold(
-            body: screens[selectedIndex],
+            body: Column(
+              children: [
+                Expanded(child: screens[selectedIndex]),
+                Builder(builder: (context) {
+                  if (emailVerified == false && codeExpired == true) {
+                    return const SendVerificationCodeSnackbar();
+                  } else if (emailVerified == false && codeExpired == false) {
+                    return const CheckEmailVerificationCodeSnackbar();
+                  } else {
+                    return const SizedBox();
+                  }
+                })
+              ],
+            ),
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class CheckEmailVerificationCodeSnackbar extends StatefulWidget {
+  const CheckEmailVerificationCodeSnackbar({
+    super.key,
+  });
+
+  @override
+  State<CheckEmailVerificationCodeSnackbar> createState() =>
+      _CheckEmailVerificationCodeSnackbarState();
+}
+
+class _CheckEmailVerificationCodeSnackbarState
+    extends State<CheckEmailVerificationCodeSnackbar> {
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Text('Enter the verification code sent to your email:'),
+        Expanded(
+          child: TextField(
+            decoration: const InputDecoration(
+              hintText: 'Verification Code',
+            ),
+            onSubmitted: isLoading
+                ? null
+                : (value) async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    functions
+                        .httpsCallable('checkEmailVerificationCode')
+                        .call({'code': value}).catchError((e) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      buildErrorAlertDialog(e);
+                      throw e;
+                    });
+                  },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SendVerificationCodeSnackbar extends StatefulWidget {
+  const SendVerificationCodeSnackbar({
+    super.key,
+  });
+
+  @override
+  State<SendVerificationCodeSnackbar> createState() =>
+      _SendVerificationCodeSnackbarState();
+}
+
+class _SendVerificationCodeSnackbarState
+    extends State<SendVerificationCodeSnackbar> {
+  bool isSending = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Text('Email not verified'),
+        const Spacer(),
+        TextButton(
+          onPressed: isSending
+              ? null
+              : () async {
+                  setState(() {
+                    isSending = true;
+                  });
+                  //send the user id to the cloud function
+                  await functions
+                      .httpsCallable('sendEmailVerification')
+                      .call()
+                      .catchError((e) {
+                    setState(() {
+                      isSending = false;
+                    });
+                    buildErrorAlertDialog(e);
+                    throw e;
+                  });
+                },
+          child: const Text('Resend'),
         ),
       ],
     );
