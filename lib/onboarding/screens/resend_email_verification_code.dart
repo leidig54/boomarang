51c511a5d633c2599b085848559a816a77a26451
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-29 19:53:29 +0000

    Refactor: Improve code readability and performance

---
+++ lib/onboarding/screens/resend_email_verification_code.dart
@@ -0,0 +1,56 @@
+import 'package:boomarang/main.dart';
+import 'package:boomarang/shared/alert_dialog.dart';
+import 'package:flutter/material.dart';
+
+class ResendEmailVerificationCodeScreen extends StatefulWidget {
+  const ResendEmailVerificationCodeScreen({
+    super.key,
+  });
+
+  @override
+  State<ResendEmailVerificationCodeScreen> createState() =>
+      _ResendEmailVerificationCodeScreenState();
+}
+
+class _ResendEmailVerificationCodeScreenState
+    extends State<ResendEmailVerificationCodeScreen> {
+  bool isSending = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+              'Your email has not been verified. Please verify your email to continue.'),
+          const SizedBox(height: 16),
+          isSending
+              ? const LinearProgressIndicator()
+              : TextButton(
+                  onPressed: () async {
+                    setState(() {
+                      isSending = true;
+                    });
+                    //send the user id to the cloud function
+                    await functions
+                        .httpsCallable('sendVerificationEmailCallable')
+                        .call()
+                        .catchError((e) {
+                      setState(() {
+                        isSending = false;
+                      });
+                      buildErrorAlertDialog(e);
+                      throw e;
+                    }).whenComplete(() {
+                      setState(() {
+                        isSending = false;
+                      });
+                    });
+                  },
+                  child: const Text('Resend verification email'),
+                ),
+        ],
+      ),
+    );
+  }
+}
