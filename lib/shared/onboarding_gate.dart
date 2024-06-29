d7e99a02fde8ccf1186d1c917c0c1339b18e9cae
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-29 20:28:41 +0000

    refactor(user profile): Refactor user profile handling and update navigation
    - Updated user profile screen renaming it from ProfileScreen to BoomarangProfileScreen
    - Improved user verification flow by preventing submissions while verifying
    - Updated user types in the selection screen to 'Healthcare Provider' and 'Insurer'
    - Adjusted navigation logic to accommodate profile completion check
    - Added TODO comment for clearing user subscription on logout

---
+++ lib/shared/onboarding_gate.dart
@@ -0,0 +1,62 @@
+import 'package:boomarang/data/user_provider.dart';
+import 'package:boomarang/onboarding/screens/enter_email_verification_code.dart';
+import 'package:boomarang/onboarding/screens/resend_email_verification_code.dart';
+import 'package:boomarang/onboarding/screens/select_user_type.dart';
+import 'package:boomarang/requester/screens/home.dart';
+import 'package:boomarang_shared/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class OnboardingGateScreen extends StatefulWidget {
+  const OnboardingGateScreen({super.key});
+
+  @override
+  State<OnboardingGateScreen> createState() => _OnboardingGateScreenState();
+}
+
+class _OnboardingGateScreenState extends State<OnboardingGateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    BoomarangUser? user = context.watch<UserProvider>().user;
+
+    if (user == null) {
+      return const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Text("Creating user profile...")
+            ],
+          ),
+        ),
+      );
+    }
+
+    //onboarding - email verification
+    if (user.emailVerified == false) {
+      if (user.emailVerificationCodeHasExpired) {
+        return const ResendEmailVerificationCodeScreen();
+      } else {
+        return const EnterEmailVerificationCodeScreen();
+      }
+    }
+
+    //onboarding - user type
+    if (user.userType == null) {
+      return const SelectUserTypeScreen();
+    }
+
+    if (user.userType == 'holder') {
+      return const RequesterHome();
+    } else if (user.userType == 'requester') {
+      return const RequesterHome();
+    } else {
+      return const Scaffold(
+        body: Center(
+          child: Text('User type not recognised. Please contact support.'),
+        ),
+      );
+    }
+  }
+}
