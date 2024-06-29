afcd5c87ba40895d5cfea7fe822dcf8d615df307
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-29 20:28:41 +0000

    fix(user-provider): Fix user handling during Firestore subscription
    - Handle user null case in Firestore snapshots
    - Update navigation and profile screen references
    - Refactor email verification code submission logic for better state management
    - Improve onboarding logic to redirect to profile completion
    - Rename ProfileScreen to BoomarangProfileScreen for clarity
    - Update user type display names in the selection screen

--- lib/shared/profile.dart
+++ lib/shared/profile.dart
@@ -91,8 +91,6 @@ class _BoomarangProfileScreenState extends State<BoomarangProfileScreen> {
                   FormBuilderTextField(
                     name: 'email',
                     readOnly: true,
-                    enableInteractiveSelection: false,
-                    enabled: false,
                     initialValue: user?.email,
                     decoration: const InputDecoration(
                       labelText: 'Email',
@@ -104,19 +102,6 @@ class _BoomarangProfileScreenState extends State<BoomarangProfileScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ElevatedButton.icon(
-                        onPressed: !_formChanged
-                            ? null
-                            : () {
-                                _userFormKey.currentState!.reset();
-                                setState(() {
-                                  _formChanged = false;
-                                });
-                              },
-                        icon: const Icon(Icons.clear),
-                        label: const Text('Reset'),
-                      ),
-                      const SizedBox(width: 16),
                       ElevatedButton.icon(
                         icon: const Icon(Icons.save),
                         onPressed: !_formChanged
@@ -137,6 +122,19 @@ class _BoomarangProfileScreenState extends State<BoomarangProfileScreen> {
                                 }
                               },
                         label: const Text('Save'),
+                      ),
+                      const SizedBox(width: 16),
+                      ElevatedButton.icon(
+                        onPressed: !_formChanged
+                            ? null
+                            : () {
+                                _userFormKey.currentState!.reset();
+                                setState(() {
+                                  _formChanged = false;
+                                });
+                              },
+                        icon: const Icon(Icons.clear),
+                        label: const Text('Discard'),
                       ),
                     ],
                   ),
