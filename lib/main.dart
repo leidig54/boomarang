a6ff82aa24317a693251011d10c3429f1550ae41
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-06-29 19:02:53 +0000

    refactor(form handling): Refactor request form to change authoriser fields to subject fields
    - Changed fields from authoriserFirstName, authoriserLastName, authoriserEmail to subjectFirstName, subjectLastName, subjectEmail in create_requests.js
    - Updated email sending logic to use subjectEmail instead of authoriserEmail in index.ts
    - Modified AddRequestScreen widget to replace authoriser related fields with subject related fields
    - Updated corresponding model properties to reflect subject rather than authoriser for better clarity in user roles
    - Modified UI labels from Authoriser to Subject for clarity across the application
    - Refactored Firebase Firestore functions and request handling logic to accommodate these changes

--- lib/main.dart
+++ lib/main.dart
@@ -315,6 +315,7 @@ class _HomeState extends State<Home> {
             if (user?.userType == 'requester')
               NavigationRailDestination(
                 icon: const Icon(Icons.add),
+                disabled: user?.userType == null,
                 label: Text(
                   'Add New',
                   style: Theme.of(context).textTheme.bodyLarge,
