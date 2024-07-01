db242f647057953a04c1877cc2862686825b2b45
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-01 15:06:14 +0000

    fix(firebase): Fix Firebase initialization and user authentication provider
    - Added UserAuthProvider to manage user authentication state.
    - Refactored main.dart to ensure Firebase is properly initialized before rendering the application.
    - Updated provider structure to use ChangeNotifier for UserAuthProvider.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,18 +4,12 @@ import { firebase } from "@genkit-ai/firebase";
 import { firebaseAuth } from "@genkit-ai/firebase/auth";
 import { onFlow } from "@genkit-ai/firebase/functions";
 import { gemini15ProPreview, vertexAI } from "@genkit-ai/vertexai";
-import * as crypto from "crypto";
 import { FieldValue, Timestamp } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
 import nodemailer from "nodemailer";
 import * as z from "zod";
 import serviceAccount from "./serviceKey.json";
 import admin = require("firebase-admin");
-
-// TODO: improve templating
-// TODO: add feedback space
-// TODO: payments - should we set the prices?
-
 
 const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
 
@@ -32,7 +26,7 @@ configureGenkit({
     firebase(),
     vertexAI({ location: "europe-west2" }),
   ],
-  logLevel: "error",
+  logLevel: "debug",
   enableTracingAndMetrics: true,
 });
 
@@ -43,7 +37,6 @@ export const generateReport = onFlow({
   },
   inputSchema: z.object({
     requestData: z.object({
-      requestType: z.string(),
       text: z.string().nullable(),
       fileUrl: z.string().nullable(),
     }),
@@ -51,7 +44,6 @@ export const generateReport = onFlow({
       text: z.string().nullable(),
       fileUrl: z.string().nullable(),
     }),
-    id: z.string(),
   }),
   outputSchema: z.string(),
   authPolicy: firebaseAuth((user) => {
@@ -61,35 +53,15 @@ export const generateReport = onFlow({
   }),
 },
 async (subject) => {
-  console.log("Generating report...");
-  // get the user id
-  const userId = subject.id;
-
-  // get the user document
-  const userDoc = admin.firestore().collection("users").doc(userId);
-
-  // get the title, firstName and lastName from the user document
-  const user = await userDoc.get();
-  if (!user.exists) {
-    throw new Error("User not found");
-  }
-  const title = user.data()?.title;
-  const firstName = user.data()?.firstName;
-  const lastName = user.data()?.lastName;
-
-
   // create the prompt for the model, given the text or the urls can be null
   const prompt = [];
   prompt.push({ text: "You are a report writing assistant. You have to write a report based on the following request details:" });
   if (subject.requestData.text) {
     prompt.push({ text: subject.requestData.text });
   }
-  // the report type is
-  prompt.push({ text: `The report type is ${subject.requestData.requestType}` });
   if (subject.requestData.fileUrl) {
     prompt.push({ media: { url: subject.requestData.fileUrl, contentType: "application/pdf" } });
   }
-
   prompt.push({ text: "You have to include the following consultation details in the report:" });
   if (subject.consultationData.text) {
     prompt.push({ text: subject.consultationData.text });
@@ -97,10 +69,7 @@ async (subject) => {
   if (subject.consultationData.fileUrl) {
     prompt.push({ media: { url: subject.consultationData.fileUrl, contentType: "application/pdf" } });
   }
-  prompt.push({ text: "Respond in raw html. Do not use ** etc. Make good use of headings or bold text to separate the components. Sign with the following details: " });
-  prompt.push({ text: `${title} ` });
-  prompt.push({ text: `${firstName} ` });
-  prompt.push({ text: `${lastName}` });
+  prompt.push({ text: "Respond in raw html. Do not use ** etc. Make good use of headings or bold text to separate the components." });
 
 
   let result;
@@ -120,87 +89,60 @@ async (subject) => {
 }
 );
 
-export const sendConsentAppWhenRequestSubmitted = functions.firestore.document("requests/{requestId}").onCreate(async (change, context) => {
+export const sendConsentAppWhenRequestSubmitted = functions.firestore.document("requests/{requestId}").onWrite(async (change, context) => {
   // we need to check if to see if the isSubmitted field is true when the request is created or updated (i.e. when the request is submitted), but we only want to send the email once, so we need to check if the isSubmitted field is true and the request has not been submitted before
+  if (change.after.data()?.isSubmitted === true && change.before.data()?.isSubmitted !== true) {
+    console.log("Request has been submitted. Sending email to subject...");
+    const request = change.after.data();
 
-  const request = change.data();
+    // get the subject email from the request
+    const subjectEmail = request?.subjectEmail;
 
-  console.log("IsDemo: ", request?.isDemo);
+    // email the subject with the request id
+    // Configure the email transport using the provided SMTP server.
+    const email = "george@joinoto.com";
+    const password = "GHD9XULrYSFwdOKM";
+    const mailTransport = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
 
-  // if isDemo, dont send email
-  if (request?.isDemo) {
-    return "Demo request";
-  }
+    const address = isEmulator ? "http://localhost:62409" : "https://boomarang-consent.web.app";
 
-  console.log("Request has been submitted. Sending email to subject...");
-
-
-  // generate a unique token for the consenting process
-  const token = crypto.randomBytes(20).toString("hex");
-
-  // create an expiry date for the token, 24 hours from now
-  const expiresAt = new Date();
-  expiresAt.setHours(expiresAt.getHours() + 24);
-
-
-  // save the token to a new document in a consent tokens collection
-  await admin.firestore().collection("consent_tokens").doc(context.params.requestId).set({
-    token,
-    createdAt: FieldValue.serverTimestamp(),
-    expiresAt: Timestamp.fromDate(expiresAt),
-  });
-
-
-  // get the subject email from the request
-  const subjectEmail = request?.subjectEmail;
-
-  // email the subject with the request id
-  // Configure the email transport using the provided SMTP server.
-  const email = "george@joinoto.com";
-  const password = "GHD9XULrYSFwdOKM";
-  const mailTransport = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    auth: {
-      user: email,
-      pass: password,
-    },
-  });
-
-  const address = isEmulator ? "http://localhost:62409" : "https://boomarang-consent.web.app";
-
-  // the website url is booomarang-consent.web.app. append the request id to the url with the name requestId.
-  const emailMessageHtml = `<p>Dear Subject,</p>
+    // the website url is booomarang-consent.web.app. append the request id to the url with the name requestId.
+    const emailMessageHtml = `<p>Dear Subject,</p>
     <p>A new consent application has been submitted. Please review the request and provide your consent.</p>
     <p>Request ID: ${context.params.requestId}</p>
-    <p>Click <a href="${address}?requestId=${context.params.requestId}&token=${token}">here</a> to provide your consent.</p>
+    <p>Click <a href="${address}?requestId=${context.params.requestId}">here</a> to review the request.</p>
     <p>Thank you.</p>`;
 
 
-  const mailOptions = {
-    from: "\"George\" <george@boomarang.com>",
-    to: subjectEmail,
-    subject: "Consent requested",
-    html: emailMessageHtml,
-  };
+    const mailOptions = {
+      from: "\"George\" <george@boomarang.com>",
+      to: subjectEmail,
+      subject: "Consent requested",
+      html: emailMessageHtml,
+    };
 
-  try {
-    await mailTransport.sendMail(mailOptions);
-    console.log(`Email sent to: ${mailOptions.to}`);
-    return null;
-  } catch (error) {
-    console.error("There was an error while sending the email:", error);
+    try {
+      await mailTransport.sendMail(mailOptions);
+      console.log(`Email sent to: ${mailOptions.to}`);
+      return null;
+    } catch (error) {
+      console.error("There was an error while sending the email:", error);
+      return null;
+    }
+  } else {
+    console.log("Request has not been submitted. Exiting...");
     return null;
   }
 });
 
 export const createUserDocument = functions.auth.user().onCreate(async (user) => {
-  // if already email verified, return
-  if (user.emailVerified) {
-    console.log("User email already verified. Is a demo user. Exiting...");
-    return null;
-  }
-
   // create the user document
   const userDoc = admin.firestore().collection("users").doc(user.uid);
   await userDoc.set({
@@ -328,10 +270,6 @@ export const sendVerificationEmail = async (userId: string, ) => {
     throw new Error("User email is null");
   }
 
-  // if isDemo, dont send email
-  if (user.data()?.isDemo) {
-    return "Demo user";
-  }
 
   // send a verification code (6 digits) to the users email
   const code = Math.floor(100000 + Math.random() * 900000);
@@ -466,122 +404,3 @@ export const markRequestAsCompleteWhenResponseSubmitted = functions.firestore.do
   return null;
 }
 );
-
-export const getFirstandLastName = functions.https.onCall(async (data) => {
-  // given the requestId, get the subjectFirstName and subjectLastName from the request document
-  const requestId = data.requestId;
-  const requestDoc = admin.firestore().collection("requests").doc(requestId);
-  const request = await requestDoc.get();
-  if (!request.exists) {
-    throw new functions.https.HttpsError("not-found", "Request not found");
-  }
-  const subjectFirstName = request.data()?.subjectFirstName;
-  const subjectLastName = request.data()?.subjectLastName;
-  return { subjectFirstName, subjectLastName };
-}
-);
-
-export const verifyDateOfBirth = functions.https.onCall(async (data) => {
-  // get the dateOfBirth and requestId from the call data, then get the dateOfBirth from the request document, and compare the two. return the result as 'verified' key in the response
-  const requestId = data.requestId;
-  const dateOfBirthISO = data.dateOfBirth;
-  const requestDoc = admin.firestore().collection("requests").doc(requestId);
-  const request = await requestDoc.get();
-  if (!request.exists) {
-    throw new functions.https.HttpsError("not-found", "Request not found");
-  }
-
-  // Convert ISO 8601 string to Timestamp
-  const submittedDateOfBirthTimestamp = new Date(dateOfBirthISO);
-
-  // Convert Firestore Timestamp to Date object
-  const requestDateOfBirthTimestamp = request.data()?.subjectDOB.toDate();
-
-  // Normalize dates to the start of the day in UTC for accurate day comparison
-  const normalizeDateToUTCStartOfDay = (date: Date) => {
-    return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
-  };
-
-  const requestDOBNormalized = normalizeDateToUTCStartOfDay(requestDateOfBirthTimestamp);
-  const submittedDOBNormalized = normalizeDateToUTCStartOfDay(submittedDateOfBirthTimestamp);
-
-  // Compare the normalized dates
-  const verified = requestDOBNormalized.getTime() === submittedDOBNormalized.getTime();
-
-  // update the request document to mark the date of birth as verified
-  if (verified) {
-    await requestDoc.update({
-      subjectDOBVerified: true,
-    });
-  }
-
-
-  console.log("Normalized Request DOB: ", requestDOBNormalized);
-  console.log("Normalized Submitted DOB: ", submittedDOBNormalized);
-  console.log("Date of birth verified: ", verified);
-
-  return { verified };
-}
-);
-
-export const verifyConsent = functions.https.onCall(async (data) => {
-  // get the consent token and requestId from the call data, then get the token from the consent tokens collection and compare the two. return the result as 'verified' key in the response
-  const requestId = data.requestId;
-  const token = data.token;
-  const consentTokenDoc = admin.firestore().collection("consent_tokens").doc(requestId);
-  const consentToken = await consentTokenDoc.get();
-  if (!consentToken.exists) {
-    throw new functions.https.HttpsError("not-found", "Consent token not found");
-  }
-
-  // check it hasn't expired
-  if (consentToken.data()?.expiresAt.toMillis() < Date.now()) {
-    throw new functions.https.HttpsError("invalid-argument", "Consent token has expired");
-  }
-
-  // update the request document to mark the consent as given
-  const requestDoc = admin.firestore().collection("requests").doc(requestId);
-  await requestDoc.update({
-    consentVerified: true,
-  });
-
-  const verified = consentToken.data()?.token === token;
-
-  return { verified };
-}
-);
-
-export const confirmEmailAddress = functions.https.onCall(async (data) => {
-  // get the requestId and the token and check if the token matches the token in the consent_tokens collection
-  const requestId = data.requestId;
-  const token = data.token;
-  const consentTokenDoc = admin.firestore().collection("consent_tokens").doc(requestId);
-  const consentToken = await consentTokenDoc.get();
-  if (!consentToken.exists) {
-    throw new functions.https.HttpsError("not-found", "Consent token not found");
-  }
-
-  // check it hasn't expired
-  if (consentToken.data()?.expiresAt.toMillis() < Date.now()) {
-    throw new functions.https.HttpsError("invalid-argument", "Consent token has expired");
-  }
-
-
-  const verified = consentToken.data()?.token === token;
-  console.log("Email verified: ", verified);
-
-  // if verified, update the subjectEmailVerified field in the request document
-  if (verified) {
-    const requestDoc = admin.firestore().collection("requests").doc(requestId);
-    await requestDoc.update({
-      subjectEmailVerified: true,
-    });
-  } else {
-    throw new functions.https.HttpsError("invalid-argument", "Invalid token");
-  }
-
-  return { verified };
-}
-);
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:boomarang/firebase_options.dart';
 import 'package:boomarang/misc/tab_index_provider.dart';
 import 'package:boomarang/requester/screens/add_request.dart';
 import 'package:boomarang/shared/auth_gate.dart';
-import 'package:boomarang_shared/theme.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
@@ -12,10 +11,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-//TODO: make stepper scrollable
-
+//TODO: move conset upload on to request details screen
+//TODO: verify the report before sending disclaimer
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFunctions functions =
     FirebaseFunctions.instanceFor(region: 'us-central1');
@@ -57,7 +57,13 @@ class _MainAppState extends State<MainApp> {
             child: MaterialApp(
               debugShowCheckedModeBanner: false,
               navigatorKey: navigatorKey,
-              theme: themeData,
+              theme: ThemeData(
+                fontFamily: GoogleFonts.balooPaaji2().fontFamily,
+                colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.indigo,
+                ),
+                scaffoldBackgroundColor: Colors.white,
+              ),
               localizationsDelegates: const [
                 GlobalMaterialLocalizations.delegate,
                 GlobalWidgetsLocalizations.delegate,
