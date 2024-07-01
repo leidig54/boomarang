243e213e23d7f75d5b6d3fa066ce8410c070d076
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-01 09:37:25 +0000

    feat(requester): Add loading screen and request submission logic
    - Implemented LoadingScreen widget for displaying loading state.
    - Created submitRequest function for handling request submissions.
    - Updated add_request.dart to include confirmation dialog before submitting requests.
    - Renamed fields and adjusted column labels in datagrid for clarity.

--- lib/holder/screens/home.dart
+++ lib/holder/screens/home.dart
@@ -1,5 +1,4 @@
-import 'package:boomarang/holder/screens/payments_datagrid.dart';
-import 'package:boomarang/holder/screens/request_datagrid.dart';
+import 'package:boomarang/holder/screens/datagrid.dart';
 import 'package:boomarang/shared/navigation_bottom_widget.dart';
 import 'package:boomarang/shared/profile.dart';
 import 'package:flutter/material.dart';
@@ -14,14 +13,11 @@ class HolderHome extends StatefulWidget {
 }
 
 class _HolderHomeState extends State<HolderHome> {
-  int selectedIndex = 1;
+  int selectedIndex = 0;
 
   List<Widget> screens = [
-    Container(),
     const HolderDatagridScreen(),
-    const HolderPaymentsDatagridScreen(),
     const BoomarangProfileScreen(),
-    Container(),
   ];
 
   @override
@@ -53,25 +49,9 @@ class _HolderHomeState extends State<HolderHome> {
           ),
           destinations: [
             NavigationRailDestination(
-              icon: const Icon(Icons.dashboard),
-              disabled: true,
-              label: Text(
-                'Dashboard',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ),
-            NavigationRailDestination(
               icon: const Icon(Icons.mail),
               label: Text(
                 'Requests',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ),
-            //payments
-            NavigationRailDestination(
-              icon: const Icon(Icons.payment),
-              label: Text(
-                'Payments',
                 style: Theme.of(context).textTheme.bodyLarge,
               ),
             ),
@@ -79,15 +59,6 @@ class _HolderHomeState extends State<HolderHome> {
               icon: const Icon(Icons.person),
               label: Text(
                 'Profile',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ),
-            //admin
-            NavigationRailDestination(
-              icon: const Icon(Icons.admin_panel_settings),
-              disabled: true,
-              label: Text(
-                'Admin',
                 style: Theme.of(context).textTheme.bodyLarge,
               ),
             ),
@@ -99,7 +70,7 @@ class _HolderHomeState extends State<HolderHome> {
               selectedIndex = index;
             });
           },
-          extended: true,
+          extended: MediaQuery.of(context).size.width > 1400,
         ),
         const VerticalDivider(
           thickness: 3,
