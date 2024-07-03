bee7dd0401161ba11092f67246759ec7b0ad9d60
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-03 08:51:12 +0000

    feat(report generation): Enhance report generation functionality by including user details and request type
    - Added requestType to the input schema for generateReport function
    - Included user ID retrieval and user details in report prompt
    - Enhanced prompt to include title, first name, and last name of the user
    - Updated error logging and debug level settings in Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
-//TODO: make stepper scrollable
-//TODO: fix delay when navgating back
+//TODO: verify the report before sending disclaimer
 
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFunctions functions =
-    FirebaseFunctions.instanceFor(region: 'europe-west2');
+    FirebaseFunctions.instanceFor(region: 'us-central1');
 FirebaseStorage storage = FirebaseStorage.instance;
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
