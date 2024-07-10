ff74ed64289dab3c723b95821717b8447e055417
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-10 09:02:22 +0000

    feat(firebase): Integrate Firebase admin functionality for enhanced data processing.
    - Removed unused files related to previous implementation.
    - Added key configuration for Firebase Admin API access.
    - Updated Firestore rules for open access to database.
    - Modified request handling to incorporate payment fees and statuses.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
+//TODO: make stepper scrollable
+
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseFunctions functions =
     FirebaseFunctions.instanceFor(region: 'us-central1');
