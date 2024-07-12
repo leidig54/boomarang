9005cdbf9340c3af7cb951b70f9694d1edea8dd7
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-12 17:33:33 +0000

    feat(holder): Add LLM explainer and request rejection dialogs
    - Modified the response handling to improve data confidentiality in the generated prompts.
    - Refactored the request datagrid to allow sorting by last name.
    - Updated the respond request screen to include new LLM explainer dialog and refactored the rejection handling.

--- lib/holder/screens/request_datagrid.dart
+++ lib/holder/screens/request_datagrid.dart
@@ -5,6 +5,7 @@ import 'package:boomarang/main.dart';
 import 'package:boomarang_shared/data/request_types.dart';
 import 'package:boomarang_shared/models/request.dart';
 import 'package:boomarang_shared/models/request_type.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
@@ -98,8 +99,7 @@ class _HolderDatagridScreenState extends State<HolderDatagridScreen> {
                 )),
             GridColumn(
                 columnName: 'subjectName',
-                allowSorting: false,
-                //TODO: Fix sorting by last name
+                allowSorting: true,
                 filterPopupMenuOptions: const FilterPopupMenuOptions(
                   canShowSortingOptions: false,
                   showColumnName: false,
@@ -132,28 +132,6 @@ class _HolderDatagridScreenState extends State<HolderDatagridScreen> {
                         .copyWith(fontWeight: FontWeight.bold),
                   ),
                 )),
-            //feePaid
-            // GridColumn(
-            //     columnName: 'fee_paid',
-            //     allowSorting: false,
-            //     allowFiltering: false,
-            //     columnWidthMode: ColumnWidthMode.fitByColumnName,
-            //     filterPopupMenuOptions: const FilterPopupMenuOptions(
-            //       canShowSortingOptions: false,
-            //       showColumnName: false,
-            //       filterMode: FilterMode.checkboxFilter,
-            //     ),
-            //     label: Container(
-            //       padding: const EdgeInsets.all(8),
-            //       alignment: Alignment.center,
-            //       child: Text(
-            //         'Paid',
-            //         style: Theme.of(context)
-            //             .textTheme
-            //             .titleMedium!
-            //             .copyWith(fontWeight: FontWeight.bold),
-            //       ),
-            //     )),
             GridColumn(
               columnName: 'requestType',
               allowSorting: true,
@@ -235,11 +213,43 @@ class HolderDataSource extends DataGridSource {
   List<DataGridRow> get rows => dataGridRows;
 
   @override
+  int compare(DataGridRow? a, DataGridRow? b, SortColumnDetails sortColumn) {
+    //if subjectName, sort by last name
+    if (sortColumn.name == 'subjectName') {
+      final String? value1 = a
+          ?.getCells()
+          .firstWhereOrNull((element) => element.columnName == sortColumn.name)
+          ?.value
+          .toString();
+      final String? value2 = b
+          ?.getCells()
+          .firstWhereOrNull((element) => element.columnName == sortColumn.name)
+          ?.value
+          .toString();
+
+      if (value1 == null || value2 == null) {
+        return 0;
+      }
+
+      final List<String> aName = value1.split(' ');
+      final List<String> bName = value2.split(' ');
+
+      final String aLastName = aName.last;
+      final String bLastName = bName.last;
+
+      if (sortColumn.sortDirection == DataGridSortDirection.ascending) {
+        return aLastName.compareTo(bLastName);
+      } else {
+        return bLastName.compareTo(aLastName);
+      }
+    }
+    return super.compare(a, b, sortColumn);
+  }
+
+  @override
   DataGridRowAdapter? buildRow(DataGridRow row) {
     return DataGridRowAdapter(
       cells: row.getCells().map<Widget>((e) {
-        //if 'received' column, format date like this
-        // 4th Jan 23, 19:00
         if (e.columnName == 'received') {
           late String formattedDateTime;
 
