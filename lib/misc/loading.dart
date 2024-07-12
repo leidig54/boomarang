e2265ed0fac19fa5fc7d986786a403fa71ffab81
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-12 18:05:18 +0000

    feat(dialogs): Add dialog components for submitting requests and reporting overwrites
    - Added OverwriteReportDialog for confirming overwrite actions in reports.
    - Added ViwewRequestType to display request type details in a dialog.
    - Updated RespondRequestScreen to use new dialog components for view and overwrite actions.
    - Created SubmitRequestDialog for submitting requests with confirmation.

--- lib/misc/loading.dart
+++ lib/misc/loading.dart
@@ -13,17 +13,15 @@ class _LoadinScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircularProgressIndicator(),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(widget.message),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(widget.message),
+        ],
       ),
     );
   }
