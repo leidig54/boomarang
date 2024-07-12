3d75483fa57b55af13bdaf94a848c4558d4f2b58
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-07-12 18:05:18 +0000

    feat(dialogs): Add Overwrite and View Request Type dialogs
    - Added OverwriteReportDialog for confirmation before overwriting reports.
    - Added ViwewRequestType dialog to display request type details.
    - Refactored RespondRequestScreen to utilize new dialog components for show and submit actions.
    - Simplified and modularized submit confirmation dialogs for requests.

---
+++ lib/holder/screens/dialogs/view_request_type.dart
@@ -0,0 +1,39 @@
+import 'package:boomarang_shared/models/request_type.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class ViwewRequestType extends StatelessWidget {
+  const ViwewRequestType({
+    super.key,
+    required this.requestType,
+  });
+
+  final RequestType requestType;
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Text(requestType.name),
+      contentPadding: const EdgeInsets.all(20),
+      children: [
+        SizedBox(
+          width: 600,
+          height: 600,
+          child: Markdown(
+            data: requestType.holderDescription!,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(20),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Close'))
+      ],
+    );
+  }
+}
