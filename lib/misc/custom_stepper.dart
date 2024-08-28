7e14fab90c083b2f45e4e352e4c86e1a50cb82ba
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-08-28 15:54:38 +0000

    feat(add_request_screen): Enhance consent form handling in AddRequestScreen
    - Centered main axis alignment in custom stepper
    - Added logic to handle user email field change for 'holder' checks
    - Updated consent form upload handling and message display
    - Refined validation for consent forms

--- lib/misc/custom_stepper.dart
+++ lib/misc/custom_stepper.dart
@@ -974,7 +974,7 @@ class _StepperState extends State<Stepper> with TickerProviderStateMixin {
                 SizedBox(
                   width: 800,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
                       Column(
