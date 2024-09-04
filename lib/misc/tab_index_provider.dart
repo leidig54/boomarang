f6c3138f28313608d8f23fbe352ae44fefe7cb96
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 16:39:04 +0000

    feat(add_request, dashboard, request_boomarang): Rename and reorganize request screens
    - Renamed 'AddRequestScreen' to 'CreateBoomarangScreen'.
    - Introduced 'DashboardScreen' with options to create or request a Boomarang.
    - Updated request dialogs to reflect new names and structure.
    - Modified layout of screens for better organization and navigation.

--- lib/misc/tab_index_provider.dart
+++ lib/misc/tab_index_provider.dart
@@ -29,7 +29,7 @@ class TabIndexProvider extends ChangeNotifier {
 
   List<Widget> screens = [
     const DashboardScreen(),
-    const InboxScreen(),
+    const ReceivedScreen(),
     const SentScreen(),
     const BoomarangProfileScreen(),
   ];
