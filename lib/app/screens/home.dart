3acaf293eb4df80733e3a3c9a8603cc4cfa0216d
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 21:52:46 +0000

    refactor(admin, lib): Refactor user creation and UI components for better consistency
    - Updated demo user creation to remove hardcoded email and uid, and to use a unified structure for users.
    - Modified dashboard and inbox screens to improve layout and text consistency.
    - Refactored import statements and utilizes a new header text style for uniformity across screens.

---
+++ lib/app/screens/home.dart
@@ -0,0 +1,92 @@
+import 'package:boomarang/misc/tab_index_provider.dart';
+import 'package:boomarang/shared/navigation_bottom_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        NavigationRail(
+          leading: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                const FlutterLogo(size: 100),
+                const SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'Boomarang',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+            //Boomerang
+          ),
+          destinations: [
+            NavigationRailDestination(
+              icon: const Icon(Icons.dashboard),
+              label: Text(
+                'Dashboard',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.inbox),
+              label: Text(
+                'Received',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.outbox),
+              label: Text(
+                'Sent',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.person),
+              label: Text(
+                'Profile',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ],
+          trailing: const NavigationRailTrailingWidget(),
+          selectedIndex: context.watch<TabIndexProvider>().tabIndex,
+          onDestinationSelected: (int index) {
+            context.read<TabIndexProvider>().setTabIndex(index);
+          },
+          extended: true,
+        ),
+        const VerticalDivider(
+          thickness: 3,
+          width: 3,
+        ),
+        Expanded(
+          child: Scaffold(
+            body: context.watch<TabIndexProvider>().screen,
+          ),
+        ),
+      ],
+    );
+  }
+}
