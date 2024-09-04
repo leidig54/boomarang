f2a277d96968812a7142dbca928e04401c0c7964
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 16:39:04 +0000

    feat(screens): Add DashboardScreen and restructure request screens
    - Renamed AddRequestScreen to CreateBoomarangScreen.
    - Added DashboardScreen allowing users to create a Boomarang request from the dashboard.
    - Updated routing to reflect screen name changes.
    - Adjusted UI elements to improve user navigation and experience.

--- lib/app/screens/inbox.dart
+++ lib/app/screens/inbox.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:boomarang/app/screens/dialogs/request_boomarang.dart';
 import 'package:boomarang/app/screens/respond_request.dart';
 import 'package:boomarang/main.dart';
 import 'package:boomarang/misc/routing_no_animation.dart';
@@ -55,19 +54,6 @@ class _ReceivedScreenState extends State<ReceivedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //request a boomarang
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return const RequestBoomarangDialog();
-              });
-        },
-        label: const Text('Request Boomarang'),
-        icon: const Icon(Icons.add_task),
-      ),
       body: SfDataGridTheme(
         data: SfDataGridThemeData(
           filterPopupTextStyle: Theme.of(context).textTheme.bodyMedium,
