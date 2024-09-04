4f4b0e6e27133979bd8bfae0596b6a40def00049
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 16:39:04 +0000

    feat(screens): Refactor request screen and add dashboard functionality
    - Renamed AddRequestScreen to CreateBoomarangScreen and adjusted related state management.
    - Modified build method to enhance UI with Dialog and PageView for better navigation.
    - Added DashboardScreen with options to create a Boomarang and request a Boomarang.
    - Updated routing to redirect to the new CreateBoomarangScreen instead of the old AddRequestScreen.

--- lib/app/screens/dashboard.dart
+++ lib/app/screens/dashboard.dart
@@ -13,71 +13,48 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 150,
-          width: double.infinity,
-          color: Theme.of(context).canvasColor,
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                "Dashboard",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
+        body: CustomScrollView(
+      slivers: [
+        const SliverAppBar.large(
+          title: Text('Dashboard'),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, top: 32, bottom: 16),
+            child: Text(
+              'Actions',
+              style: Theme.of(context).textTheme.titleLarge,
             ),
           ),
         ),
-        //actions
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
-          child: Text(
-            'Actions',
-            style: Theme.of(context).textTheme.titleLarge,
+        SliverList(
+            delegate: SliverChildListDelegate([
+          ListTile(
+            title: const Text('Create a Boomarang'),
+            subtitle: const Text(
+                'Want to make a request? Create a new Boomarang to send to a recipient.'),
+            leading: const Icon(Icons.send),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) => const CreateBoomarangScreen(),
+              );
+            },
           ),
-        ),
-        ListTile(
-          title: const Text('Create New'),
-          subtitle:
-              const Text('Want to request sensitive data? Send a Boomarang.'),
-          leading: const Icon(Icons.send),
-          onTap: () {
-            showDialog(
-              context: context,
-              builder: (context) => const CreateBoomarangScreen(),
-            );
-          },
-        ),
-
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
-          child: Text(
-            'Share',
-            style: Theme.of(context).textTheme.titleLarge,
+          ListTile(
+            title: const Text('Request a Boomarang'),
+            subtitle: const Text(
+                "Useful if you've receive a paper request and want the sender to resubmit it via Boomarang"),
+            leading: const Icon(Icons.add_task),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const RequestBoomarangScreen();
+                  });
+            },
           ),
-        ),
-        ListTile(
-          title: const Text('Request Boomarang'),
-          subtitle: const Text(
-              "If you've received a paper request and want it submitted via Boomarang"),
-          leading: const Icon(Icons.add_task),
-          onTap: () {
-            showDialog(
-              context: context,
-              builder: (context) => const RequestBoomarangScreen(),
-            );
-          },
-        ),
-        ListTile(
-          title: const Text('Share Link'),
-          subtitle: const Text(
-              "Share your boomarang link with others to allow them to request data from you."),
-          leading: const Icon(Icons.share),
-          onTap: () {},
-        ),
+        ])),
       ],
     ));
   }
