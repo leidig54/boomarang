88c4b2983b0add1178d4b0da8d41a3f7722d3537
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 14:55:17 +0000

    refactor(admin/create_demo): Refactor demo environment creation process to improve readability and consistency
    - Cleaned up code formatting for better readability
    - Replaced single quotes with double quotes for string literals
    - Consolidated comments for clarity
    - Removed redundant spacing and comments for a cleaner code structure
    - Ensured consistent naming in method parameters and corrected the variable names to fit the new structure.

--- lib/misc/tab_index_provider.dart
+++ lib/misc/tab_index_provider.dart
@@ -1,5 +1,6 @@
-import 'package:boomarang/requester/screens/add_request.dart';
-import 'package:boomarang/requester/screens/datagrid.dart';
+import 'package:boomarang/app/screens/add_request.dart';
+import 'package:boomarang/app/screens/inbox.dart';
+import 'package:boomarang/app/screens/sent.dart';
 import 'package:boomarang/shared/profile.dart';
 import 'package:boomarang_shared/models/request.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +27,7 @@ class TabIndexProvider extends ChangeNotifier {
 
   List<Widget> screens = [
     const AddRequestScreen(),
+    const HolderDatagridScreen(),
     const RequesterDatagridScreen(),
     const BoomarangProfileScreen(),
   ];
