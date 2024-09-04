299cc02493eea3886256ab26db52ca6959959714
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 22:52:46 +0100

    better

--- admin/create_demo.js
+++ admin/create_demo.js
@@ -34,11 +34,10 @@ async function createDemoEnvironment() {
     requests.docs.map((request) => requestCollection.doc(request.id).delete())
   );
 
-  const requesterId = "1";
   await auth
     .createUser({
-      uid: requesterId,
-      email: "ian@insurance.com",
+      uid: "1",
+      email: "georgeleidig@icloud.com",
       password: "boomarang",
       emailVerified: true,
     })
@@ -46,43 +45,17 @@ async function createDemoEnvironment() {
       console.log("requester created");
     });
 
-  const holderId = "2";
-  await auth
-    .createUser({
-      uid: holderId,
-      email: "ed@doctors.com",
-      password: "boomarang",
-      emailVerified: true,
-    })
-    .then((user) => {
-      console.log("holder created");
-    });
-
-  const holderUser = {
+  const user = {
     title: "Dr",
-    email: "ed@doctors.com",
-    firstName: "Ed",
-    lastName: "Farrar",
-    userType: "holder",
+    email: "georgeleidig@icloud.com",
+    firstName: "George",
+    lastName: "Leidig",
     emailVerified: true,
     verificationCodeExpiresAt: null,
     isDemo: true,
   };
 
-  const requesterUser = {
-    title: "Dr",
-    email: "ian@insurance.com",
-    firstName: "Ian",
-    lastName: "Jones",
-    userType: "requester",
-    emailVerified: true,
-    verificationCodeExpiresAt: null,
-    isDemo: true,
-  };
-
-  await usersCollection.doc(requesterId).set(requesterUser);
-  console.log("Requester document created");
-  await usersCollection.doc(holderId).set(holderUser);
+  await usersCollection.doc("1").set(user);
   console.log("Holder document created");
 
   const createRequest = async () => {
@@ -104,52 +77,16 @@ async function createDemoEnvironment() {
       "other": ["self"],
     };
 
-    const requesterFees = {
-      "private_medical_insurance": 90.0,
-      "dwp_pip": 0.0,
-      "disability_living_allowance": 0.0,
-      "police_report": 60.0,
-      "dwp_uc113": 0.0,
-      "disability_student_allowance": 0.0,
-      "subject_access_request": 0.0,
-      "other": 120.0,
-    };
-
     //get a random request type from the map
     const requestType = faker.helpers.arrayElement(Object.keys(requesterOrgs));
-
-    //get a random org from the array of orgs for the request type
-    const org = faker.helpers.arrayElement(requesterOrgs[requestType]);
-
-    //get the fee for the request type
-    const fee = requesterFees[requestType];
-
-    //feePaid is null for requests that cost 0, otherwise is a 50/50 chance of being true or false
-    if (fee === 0) {
-      feePaid = null;
-    } else {
-      //dont use faker here as we want a 50/50 chance
-      feePaid = Math.random() < 0.5;
-    }
-
-    const potentialPayers = ["requester", "subject"];
 
     //for 10% of requests, set the status to rejected. for the others, set to awaiting_response
     const requestStatus =
       Math.random() < 0.1 ? "rejected" : "awaiting_response";
 
-    let requestDetails;
-    let requestFormRef;
-
-    //if the requesterOrg is self, set the request details to lorem ipsum, 4 to 8 sentences, and set the form ref to null
-    if (org === "self") {
-      requestDetails = faker.lorem.sentences(Math.floor(Math.random() * 4) + 4);
-      requestFormRef = null;
-    } else {
-      requestDetails = null;
-      requestFormRef =
-        "https://firebasestorage.googleapis.com/v0/b/boomarang-ac130.appspot.com/o/demo%2FRequest%20Details.pdf?alt=media&token=67700154-72bf-4e97-b5b1-10891d54b7b2";
-    }
+    let requestDetails = faker.lorem.sentences(
+      Math.floor(Math.random() * 4) + 4
+    );
 
     const request = {
       id: id,
@@ -159,25 +96,18 @@ async function createDemoEnvironment() {
       subjectEmailVerified: true,
       subjectDOB: faker.date.past(),
       subjectDOBVerified: true,
-      requesterUserId: requesterId,
-      requesterEmail: requesterUser.email,
-      requesterOrgName: org,
-      holderUserId: holderId,
+      senderUserId: "1",
+      senderEmail: user.email,
+      recipientUserId: "1",
+      recipientEmail: user.email,
       dateCreated: faker.date.recent({
         days: 4,
       }),
-      consentVerified: true,
+      consentVerified: Math.random() < 0.5,
       requestStatus: requestStatus,
       requestType: requestType,
       requestDetails: requestDetails,
-      requestFormRef: requestFormRef,
-      consentFormRef:
-        "https://firebasestorage.googleapis.com/v0/b/boomarang-ac130.appspot.com/o/demo%2FConsent%20Form.pdf?alt=media&token=655d6b8c-e21c-4dcf-9008-74941eebea71",
       isDemo: true,
-      fee: fee,
-      feePaid: feePaid,
-      paymentDate: feePaid ? faker.date.recent() : null,
-      payer: fee != 0 ? faker.helpers.arrayElement(potentialPayers) : null,
     };
 
     await requestCollection.doc(request.id).set(request);
