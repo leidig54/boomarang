d16a7ae58110b6b05bee00a7e1d646f9636473c2
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 15:17:42 +0000

    refactor(app/screens): Renamed holder to recipient and requester to sender throughout the app
    - Renamed instances of 'holder' to 'recipient' in AddRequestScreen and other relevant files
    - Updated field names and keys related to holder/requester throughout the application
    - Removed DashboardScreen implementation as it was unused

---
+++ lib/app/logic/generate_report.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:boomarang/main.dart';
+import 'package:boomarang/shared/alert_dialog.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
+import 'package:flutter_quill_delta_from_html/flutter_quill_delta_from_html.dart';
+
+Future<Document> generateReport({
+  required RequestData requestData,
+  required ConsultationData consultationData,
+}) async {
+  final HttpsCallable callable = functions.httpsCallable('generateReport');
+
+  final HttpsCallableResult results = await callable.call(
+    <String, dynamic>{
+      'requestData': {
+        'text': requestData.text,
+        'requestType': requestData.requestType,
+      },
+      'consultationData': {
+        'text': consultationData.text,
+        'fileUrl': consultationData.file,
+      },
+      'id': auth.currentUser?.uid,
+    },
+  ).catchError((error) {
+    String errorText = "Error generating report: $error";
+    buildErrorAlertDialog(errorText);
+    throw error;
+  });
+
+  //return the generated report
+  String reportTextHtml = results.data;
+
+  debugPrint('Generated report: $reportTextHtml');
+
+  Delta reportTextDelta = HtmlToDelta().convert(reportTextHtml);
+
+  return Document.fromDelta(reportTextDelta);
+}
+
+class RequestData {
+  String? text;
+  String requestType;
+
+  RequestData({
+    this.text,
+    required this.requestType,
+  });
+}
+
+class ConsultationData {
+  String? text;
+  String? file;
+
+  ConsultationData({this.text, this.file});
+}
