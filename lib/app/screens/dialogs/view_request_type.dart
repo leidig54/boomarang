5447a4d23e942194ef0b135f70b423082884bb02
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-04 15:17:42 +0000

    refactor(undefined): Rename holder references to recipient
    - Renamed holder details forms and validation keys to recipient in AddRequestScreen.
    - Updated field names from 'to_email' to 'recipient_email'.
    - Adjusted datagrid and state class names from HolderDatagridScreen to ReceivedScreen and RequesterDatagridScreen to SentScreen.
    - Changed references from holder to recipient in relevant dialogs and linear responses.

--- lib/app/screens/dialogs/view_request_type.dart
+++ lib/app/screens/dialogs/view_request_type.dart
@@ -20,7 +20,7 @@ class ViwewRequestType extends StatelessWidget {
           width: 600,
           height: 600,
           child: Markdown(
-            data: requestType.holderDescription!,
+            data: requestType.description!,
             shrinkWrap: true,
             padding: const EdgeInsets.all(20),
           ),
