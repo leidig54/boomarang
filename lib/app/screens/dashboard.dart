d0a425a9636c55292ada5cc6c68fba642931c411
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-05 09:50:48 +0000

    feat(dashboard): Add share functionality and update dashboard actions
    - Updated padding for action items
    - Renamed 'Create a Boomarang' to 'Create New'
    - Modified subtitle text for creating and requesting Boomarang
    - Added 'Share' section with new functionality to share a link

--- lib/app/screens/dashboard.dart
+++ lib/app/screens/dashboard.dart
@@ -33,16 +33,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
         ),
         //actions
         Padding(
-          padding: const EdgeInsets.only(left: 16.0, bottom: 16.0, top: 32.0),
+          padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
           child: Text(
             'Actions',
             style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
         ListTile(
-          title: const Text('Create a Boomarang'),
-          subtitle: const Text(
-              'Want to request sensitive data? Create a new Boomarang.'),
+          title: const Text('Create New'),
+          subtitle:
+              const Text('Want to request sensitive data? Send a Boomarang.'),
           leading: const Icon(Icons.send),
           onTap: () {
             showDialog(
@@ -51,18 +51,32 @@ class _DashboardScreenState extends State<DashboardScreen> {
             );
           },
         ),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
+          child: Text(
+            'Share',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
         ListTile(
-          title: const Text('Request a Boomarang'),
+          title: const Text('Request Boomarang'),
           subtitle: const Text(
-              "Useful if you've receive a paper request and want the sender to resubmit it via Boomarang."),
+              "If you've received a paper request and want it submitted via Boomarang"),
           leading: const Icon(Icons.add_task),
           onTap: () {
             showDialog(
-                context: context,
-                builder: (context) {
-                  return const RequestBoomarangScreen();
-                });
+              context: context,
+              builder: (context) => const RequestBoomarangScreen(),
+            );
           },
+        ),
+        ListTile(
+          title: const Text('Share Link'),
+          subtitle: const Text(
+              "Share your boomarang link with others to allow them to request data from you."),
+          leading: const Icon(Icons.share),
+          onTap: () {},
         ),
       ],
     ));
