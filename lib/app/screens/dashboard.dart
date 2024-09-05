695ddf597232dfede60a92404adb78f5a76ba735
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-05 09:50:48 +0000

    feat(dashboard): Enhance dashboard screen with new actions and updates
    - Updated padding for the actions section
    - Changed title and subtitle for creating a new Boomarang
    - Added a new 'Share' section with titles and subtitles for sharing links
    - Refined the request Boomarang ListTile title and subtitle

--- lib/app/screens/dashboard.dart
+++ lib/app/screens/dashboard.dart
@@ -1,10 +1,6 @@
 import 'package:boomarang/app/screens/add_request.dart';
 import 'package:boomarang/app/screens/dialogs/request_boomarang.dart';
-import 'package:boomarang/data/user_provider.dart';
-import 'package:boomarang/misc/tab_index_provider.dart';
-import 'package:boomarang_shared/models/user.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -16,10 +12,6 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    BoomarangUser? user = context.watch<UserProvider>().user;
-    bool profileIsComplete = user?.profileIsComplete ?? false;
-    bool hasReadGuide = user?.flags?['readGuide'] ?? false;
-
     return Scaffold(
         body: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -39,40 +31,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
-          child: Text(
-            'Get Started',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        ListTile(
-          title: const Text('Complete Profile'),
-          enabled: !profileIsComplete,
-          subtitle: const Text(
-              'Complete your profile to share with others when requesting data.'),
-          leading: profileIsComplete
-              ? const Icon(Icons.check_box)
-              : const Icon(Icons.check_box_outline_blank),
-          onTap: () {
-            context.read<TabIndexProvider>().setTabIndex(3);
-          },
-        ),
-        ListTile(
-          title: const Text('Read The Guide'),
-          enabled: !hasReadGuide,
-          subtitle: const Text(
-              'Learn how to use Boomarang to request and share data.'),
-          leading: hasReadGuide
-              ? const Icon(Icons.check_box)
-              : const Icon(Icons.check_box_outline_blank),
-          onTap: () {},
-        ),
         //actions
         Padding(
           padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
           child: Text(
-            'Send',
+            'Actions',
             style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
@@ -92,15 +55,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
         Padding(
           padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
           child: Text(
-            'Receive',
+            'Share',
             style: Theme.of(context).textTheme.titleLarge,
           ),
         ),
         ListTile(
-          title: const Text('Letter'),
+          title: const Text('Request Boomarang'),
           subtitle: const Text(
-              "Received a paper request? We'll help you request it via Boomarang instead."),
-          leading: const Icon(Icons.mail),
+              "If you've received a paper request and want it submitted via Boomarang"),
+          leading: const Icon(Icons.add_task),
           onTap: () {
             showDialog(
               context: context,
@@ -109,27 +72,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
           },
         ),
         ListTile(
-          title: const Text('Website'),
+          title: const Text('Share Link'),
           subtitle: const Text(
-              "Receive Boomarangs on your website - get a link to your portal here."),
-          leading: const Icon(Icons.web),
+              "Share your boomarang link with others to allow them to request data from you."),
+          leading: const Icon(Icons.share),
           onTap: () {},
-        ),
-        //copy email forwarding link
-        ListTile(
-          title: const Text('Email'),
-          subtitle: const Text(
-              "Forward email requests to a dedicated inbox and we'll take care of the rest."),
-          leading: const Icon(Icons.alternate_email),
-          onTap: () {
-            //copy email to clipboard
-            //TODO: Create a function to copy text to clipboard
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Email copied to clipboard'),
-              ),
-            );
-          },
         ),
       ],
     ));
