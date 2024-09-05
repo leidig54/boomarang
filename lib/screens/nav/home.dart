ed396f7f2dcf6d06ec4a5937e9eb7a66827488f8
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-05 20:24:36 +0000

    refactor(logic): Remove outdated report generation and request submission logic
    - Removed generate_report.dart, submit_request.dart, and related dialog screens.
    - Modified import paths to reflect new organization of shared components.
    - Renamed various files for better clarity and structure.

--- lib/screens/nav/home.dart
+++ lib/screens/nav/home.dart
@@ -1,5 +1,5 @@
+import 'package:boomarang/misc/navigation_bottom_widget.dart';
 import 'package:boomarang/providers/tab_index_provider.dart';
-import 'package:boomarang/screens/misc/navigation_bottom_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
