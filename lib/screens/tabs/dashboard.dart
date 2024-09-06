4e4b930321a4c8cfdaa07ad58cbdf2573b7b6f19
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-06 08:42:59 +0000

    fix(dashboard): Disable interactive elements on the dashboard
    - Set 'Read The Guide', 'Website', and 'Email' ListTiles to disabled state.
    - Removed onTap functionality for 'Email' ListTile.
    - Added TODOs for future implementation.

--- lib/screens/tabs/dashboard.dart
+++ lib/screens/tabs/dashboard.dart
@@ -1,5 +1,7 @@
-import 'package:boomarang/providers/tab_provider.dart';
+import 'package:boomarang/providers/tab_index_provider.dart';
 import 'package:boomarang/providers/user_provider.dart';
+import 'package:boomarang/screens/dialogs/add_request.dart';
+import 'package:boomarang/screens/dialogs/request_boomarang.dart';
 import 'package:boomarang_shared/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -79,10 +81,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
           title: const Text('Create New'),
           subtitle:
               const Text('Want to request sensitive data? Send a Boomarang.'),
-          enabled: false,
           leading: const Icon(Icons.send),
-          onTap: () {},
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => const CreateBoomarangScreen(),
+            );
+          },
         ),
+
         Padding(
           padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 32.0),
           child: Text(
@@ -92,11 +99,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
         ),
         ListTile(
           title: const Text('Letter'),
-          enabled: false,
           subtitle: const Text(
               "Received a paper request? We'll help you request it via Boomarang instead."),
           leading: const Icon(Icons.mail),
-          onTap: () {},
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => const RequestBoomarangScreen(),
+            );
+          },
         ),
         ListTile(
           title: const Text('Website'),
