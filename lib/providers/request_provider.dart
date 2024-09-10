cd8bcddc1973d93f058a0c2747ecd64bf40660e8
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-10 16:29:39 +0000

    feat(inbox): refactor inbox and sentbox to enhance request handling
    - Added Inbox and Sentbox providers for better state management.
    - Refactored the request handling logic in the inbox and sent tabs.
    - Created request meta and request form components for organized UI structure. Improved UI with better state indication.

--- lib/providers/request_provider.dart
+++ lib/providers/request_provider.dart
@@ -6,9 +6,7 @@ import 'package:flutter/material.dart';
 
 class RequestProvider extends ChangeNotifier {
   List<BoomarangRequest> receivedRequests = [];
-  List<BoomarangRequest> sentRequests = [];
   late StreamSubscription receivedRequestStreamSubscription;
-  late StreamSubscription sentRequestStreamSubscription;
 
   RequestProvider() {
     receivedRequestStreamSubscription = firestore
@@ -19,18 +17,6 @@ class RequestProvider extends ChangeNotifier {
       receivedRequests =
           snapshot.docs.map((e) => BoomarangRequest.fromMap(e.data())).toList();
       receivedRequests.sort((a, b) {
-        return b.dateCreated.compareTo(a.dateCreated);
-      });
-      notifyListeners();
-    });
-    sentRequestStreamSubscription = firestore
-        .collection('requests')
-        .where('senderUserId', isEqualTo: auth.currentUser!.uid)
-        .snapshots()
-        .listen((snapshot) {
-      sentRequests =
-          snapshot.docs.map((e) => BoomarangRequest.fromMap(e.data())).toList();
-      sentRequests.sort((a, b) {
         return b.dateCreated.compareTo(a.dateCreated);
       });
       notifyListeners();
