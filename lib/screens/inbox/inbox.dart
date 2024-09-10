4beb2fe884f60fdffd41afbc5d247e7a94164630
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-10 17:30:46 +0000

    refactor(inbox and request handling): Refactor inbox and request management by consolidating providers and removing unused code.
    - Renamed InboxProvider and SentboxProvider to RequestProvider.
    - Created a unified RequestProvider to handle requests.
    - Removed unused dialog screens and multiple provider classes.
    - Updated related screens to use the new RequestProvider.

---
+++ lib/screens/inbox/inbox.dart
@@ -0,0 +1,144 @@
+import 'package:boomarang/providers/request_provider.dart';
+import 'package:boomarang/screens/inbox/form.dart';
+import 'package:boomarang/screens/inbox/meta.dart';
+import 'package:boomarang/screens/inbox/tile.dart';
+import 'package:boomarang_shared/models/request.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class InboxScreen extends StatefulWidget {
+  const InboxScreen({super.key});
+
+  @override
+  State<InboxScreen> createState() => _InboxScreenState();
+}
+
+class _InboxScreenState extends State<InboxScreen> {
+  BoomarangRequest? selectedRequest;
+  bool hideSubmitted = false;
+
+  @override
+  void didChangeDependencies() {
+    selectedRequest ??=
+        context.read<RequestProvider>().receivedRequests.firstOrNull;
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 150,
+          width: double.infinity,
+          color: Theme.of(context).canvasColor,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Inbox",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ),
+        ),
+        const Divider(
+          height: 1,
+          thickness: 0.5,
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Container(
+                constraints: const BoxConstraints(
+                  maxWidth: 300,
+                  minWidth: 100,
+                ),
+                child: Column(
+                  children: [
+                    CheckboxListTile(
+                        value: hideSubmitted,
+                        tileColor: Theme.of(context).canvasColor,
+                        onChanged: (value) {
+                          setState(() {
+                            hideSubmitted = value ?? false;
+                          });
+                        },
+                        title: const Text("Hide Submitted"),
+                        controlAffinity: ListTileControlAffinity.trailing),
+                    const Divider(
+                      height: 1,
+                      thickness: 0.5,
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                          vertical: 16.0,
+                        ),
+                        itemBuilder: (context, index) {
+                          BoomarangRequest thisRequest = context
+                              .watch<RequestProvider>()
+                              .receivedRequests[index];
+
+                          if (hideSubmitted && thisRequest.responseSubmitted) {
+                            return Container();
+                          }
+
+                          return RequestTile(
+                            tileRequest: thisRequest,
+                            isSelected: thisRequest.id == selectedRequest?.id,
+                            onTap: (request) {
+                              setState(() {
+                                selectedRequest = thisRequest;
+                              });
+                            },
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          BoomarangRequest thisRequest = context
+                              .watch<RequestProvider>()
+                              .receivedRequests[index];
+
+                          if (hideSubmitted && thisRequest.responseSubmitted) {
+                            return Container();
+                          }
+                          return const Divider(
+                            indent: 8,
+                            endIndent: 8,
+                          );
+                        },
+                        itemCount: context
+                            .watch<RequestProvider>()
+                            .receivedRequests
+                            .length,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const VerticalDivider(
+                width: 1,
+              ),
+              Expanded(
+                child: selectedRequest == null
+                    ? Container()
+                    : RequestMeta(selectedRequest: selectedRequest!),
+              ),
+              const VerticalDivider(
+                width: 1,
+              ),
+              Expanded(
+                flex: 2,
+                child: selectedRequest == null
+                    ? Container()
+                    : RequestForm(selectedRequest: selectedRequest!),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
