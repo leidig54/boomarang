100738ce7b97f4f95ddf1865ba5ee16719e89c0c
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-13 10:52:36 +0000

    feat(requests): Add support for demo environment and refactor request handling
    - Enabled demo environment option in create_demo.js
    - Modified request creation logic to handle demo users
    - Extracted common request metadata display into shared components
    - Renamed and organized inbox and sent screens to improve clarity and maintainability

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -134,7 +134,9 @@ export const createUserDocument = functions
     return null;
   });
 
-// when a request is updated and the recipientEmail field is no longer null or has changed, check to see if the user with that recipientEmail already exists. if it does, assign the userId to the recipientUserId field in the request
+// when a request is updated and the recipientEmail field is no longer null or has changed, check to see if the user with that recipientEmail already exists.
+// if it does, assign the userId to the recipientUserId field in the request
+// if it doesn't, email the recipient email to create an account
 export const assignRecipientToRequestOnRequestCreate = functions
   .region("europe-west2")
   .firestore.document("requests/{requestId}")
@@ -163,6 +165,10 @@ export const assignRecipientToRequestOnRequestCreate = functions
           console.log(
             "User does not exist. Sending email to recipient to create an account..."
           );
+
+          if (change.after.data()?.isDemo) {
+            return "Is Demo";
+          }
           // Configure the email transport using the provided SMTP server.
           const email = "george@joinoto.com";
           const password = "GHD9XULrYSFwdOKM"; // Ensure you're securely handling passwords and sensitive information
@@ -209,7 +215,9 @@ export const assignRecipientToRequestOnRequestCreate = functions
     return null;
   });
 
-// when a request is added by a recipient and the sender already exists, assign the senderUserId to the request. otherwise email them to create an account
+// when a request is added by a recipient and the sender already exists, assign the senderUserId to the request.
+// otherwise email them to create an account
+// this is for manually adding paper requests
 export const assignSenderToRequestOnRequestCreate = functions
   .region("europe-west2")
   .firestore.document("requests/{requestId}")
@@ -238,6 +246,10 @@ export const assignSenderToRequestOnRequestCreate = functions
           console.log(
             "User does not exist. Sending email to sender to create an account..."
           );
+
+          if (change.after.data()?.isDemo) {
+            return "Is Demo";
+          }
           // Configure the email transport using the provided SMTP server.
           // email the sender with a link to sign up
           // Configure the email transport using the provided SMTP server.
@@ -292,7 +304,8 @@ export const assignSenderToRequestOnRequestCreate = functions
     return null;
   });
 
-// when a user is verified, check if the user has a request with a recipientEmail that matches the user's email. if it does, assign the userId to the recipientUserId field in the request
+// when a user is verified, check if the user has a request with a recipientEmail that matches the user's email.
+// if it does, assign the userId to the recipientUserId field in the request
 export const assignRequestToUserOnUserVerification = functions
   .region("europe-west2")
   .firestore.document("users/{userId}")
@@ -511,20 +524,6 @@ export const checkEmailVerificationCode = functions
     return "Email verified";
   });
 
-export const markRequestAsCompleteWhenResponseSubmitted = functions
-  .region("europe-west2")
-  .firestore.document("responses/{responseId}")
-  .onCreate(async (change) => {
-    // when a response is created, mark the request as complete
-    const response = change.data();
-    const requestId = response.id;
-    const requestDoc = admin.firestore().collection("requests").doc(requestId);
-    await requestDoc.update({
-      requestStatus: "complete",
-    });
-    return null;
-  });
-
 export const getFirstandLastName = functions
   .region("europe-west2")
   .https.onCall(async (data) => {
@@ -737,3 +736,5 @@ export const submitResponse = functions
 
     return "Response saved";
   });
+
+// TODO: Extract send email function
