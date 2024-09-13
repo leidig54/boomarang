b450c033d92341668dcc12bbbadb65476c02d68b
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-13 11:22:19 +0000

    refactor(screens/inbox and screens/sent): Refactor request retrieval using collection package
    - Updated request retrieval from firstWhere to firstWhereOrNull for safer null handling
    - Added imports for collection package in inbox.dart and sent.dart
    - Refactored ListView in sent.dart for better readability

--- lib/screens/inbox/inbox.dart
+++ lib/screens/inbox/inbox.dart
@@ -3,6 +3,7 @@ import 'package:boomarang/screens/inbox/form.dart';
 import 'package:boomarang/screens/shared/meta.dart';
 import 'package:boomarang/screens/shared/tile.dart';
 import 'package:boomarang_shared/models/request.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,7 +27,7 @@ class _InboxScreenState extends State<InboxScreen> {
       selectedRequest = context
           .read<RequestProvider>()
           .receivedRequests
-          .firstWhere((e) => e.id == selectedRequest!.id);
+          .firstWhereOrNull((e) => e.id == selectedRequest!.id);
     }
     super.didChangeDependencies();
   }
