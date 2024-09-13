3b149d7af87d7f1819667b6a163d68e2d8779bd4
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-13 10:52:36 +0000

    feat(demo): Enable demo user functionality and enhance request handling
    - Set 'withUser' to true in create_demo.js to support demo user creation.
    - Refactor request handling to send email prompts for account creation if the recipient does not exist.
    - Add a new form for displaying sent requests with non-editable fields for improved user experience.
    - Consolidate request metadata display into a shared component for reusability in both inbox and sent screens.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
-//TODO: Provide request templates
+//TODO: View and respond to received requests
+//TODO: View and cancel sent requests
+//TODO: Establish request types
 
 bool useEmulators = true;
 
--- lib/screens/sent/form.dart
+++ lib/screens/sent/form.dart
@@ -22,17 +22,18 @@ class _SentRequestFormState extends State<SentRequestForm> {
   @override
   Widget build(BuildContext context) {
     BoomarangRequest request = widget.selectedRequest;
+    print(request.response);
 
     return FormBuilder(
       key: formKey,
       child: ListView(
         padding: const EdgeInsets.all(32.0),
         children: [
-          if (request.elements != null && request.elements!.isNotEmpty == true)
+          if (request.form.elements.isNotEmpty == true)
             ...List.generate(
-              request.elements!.length,
+              request.form.elements.length,
               (index) {
-                BoomarangElement element = request.elements![index];
+                FormElement element = request.form.elements[index];
 
                 if (element.type == "text") {
                   dynamic initialValue;
@@ -42,6 +43,7 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderTextField(
+                      key: Key(element.id),
                       name: element.id,
                       readOnly: true,
                       enabled: false,
@@ -60,6 +62,7 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderCheckbox(
+                      key: Key(element.id),
                       name: element.id,
                       enabled: false,
                       onChanged: null,
@@ -79,6 +82,7 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderRadioGroup(
+                      key: Key(element.id),
                       name: element.id,
                       enabled: false,
                       initialValue: initialValue,
