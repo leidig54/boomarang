d8f3b461ce558a3062de8f31d14868b81bec0921
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-17 10:18:51 +0000

    feat(admin): Add B2B forms for various requests
    - Introduced a new array of b2bForms including requests for Employee Background Check, Medical Records for Insurance Claim, Academic Transcript, Rental Payment History, and Client Financial Records.
    - Updated create_demo.js to utilize these forms when creating demo requests.
    - Modified the UI to accommodate and display the new form structure, enhancing user experience in request creation and viewing.

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -1,7 +1,17 @@
+import 'package:boomarang/data/templates.dart';
+import 'package:boomarang/main.dart';
+import 'package:boomarang/providers/tab_provider.dart';
+import 'package:boomarang_shared/dob_formatter.dart';
 import 'package:boomarang_shared/models/boomarang_element.dart';
+import 'package:boomarang_shared/models/request.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class CreateNewRequest extends StatefulWidget {
   const CreateNewRequest({
@@ -13,92 +23,194 @@ class CreateNewRequest extends StatefulWidget {
 }
 
 class _CreateNewRequestState extends State<CreateNewRequest> {
-  List<BoomarangElement> elements = [];
+  List<FormElement> elements = [];
   //formbuilder key
   final _formKey = GlobalKey<FormBuilderState>();
+  List<String> forms = ['Form 1', 'Form 2', 'Form 3', 'Form 4'];
+  bool isSaving = false;
+  late String id;
+
+  @override
+  void initState() {
+    super.initState();
+    id = const Uuid().v4();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: FormBuilder(
-        key: _formKey,
-        child: Row(
-          children: [
-            Container(
-              constraints: const BoxConstraints(
-                maxWidth: 600,
-              ),
-              child: Column(
-                children: [
-                  ExpansionTile(
-                    title: const Text("Subject"),
-                    subtitle: const Text("Incomplete"),
-                    leading: const Icon(Icons.horizontal_rule),
-                    initiallyExpanded: true,
-                    children: [
-                      //name, dob and email fields
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: [
-                            FormBuilderTextField(
-                              name: 'first_name',
-                              decoration: const InputDecoration(
-                                  labelText: 'First Name',
-                                  border: OutlineInputBorder()),
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(),
-                                FormBuilderValidators.minLength(3),
-                              ]),
-                            ),
-                            const SizedBox(height: 16),
-                            FormBuilderTextField(
-                              name: 'last_name',
-                              decoration: const InputDecoration(
-                                labelText: 'Last Name',
-                                border: OutlineInputBorder(),
-                              ),
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(),
-                                FormBuilderValidators.minLength(3),
-                              ]),
-                            ),
-                            const SizedBox(height: 16),
-                            FormBuilderTextField(
-                              name: 'email',
-                              decoration: const InputDecoration(
-                                labelText: 'Email',
-                                border: OutlineInputBorder(),
-                              ),
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(),
-                                FormBuilderValidators.email(),
-                              ]),
-                            ),
-                            const SizedBox(height: 16),
-                            //dob
-                            FormBuilderDateTimePicker(
-                              name: 'dob',
-                              inputType: InputType.date,
-                              initialEntryMode: DatePickerEntryMode.input,
-                              decoration: const InputDecoration(
-                                labelText: 'Date of Birth',
-                                border: OutlineInputBorder(),
-                              ),
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(),
-                              ]),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+    return FormBuilder(
+      key: _formKey,
+      child: Row(
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxWidth: 600,
             ),
-          ],
-        ),
+            child: ListView(
+              padding: const EdgeInsets.all(32),
+              children: [
+                const Text("Subject"),
+                const SizedBox(height: 16),
+                FormBuilderTextField(
+                  name: 'subject_first_name',
+                  initialValue: kDebugMode ? 'John' : null,
+                  decoration: const InputDecoration(
+                      labelText: 'First Name', border: OutlineInputBorder()),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(3),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                FormBuilderTextField(
+                  name: 'subject_last_name',
+                  initialValue: kDebugMode ? 'Doe' : null,
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.minLength(3),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                FormBuilderTextField(
+                  name: 'subject_email',
+                  initialValue: kDebugMode ? 'georgeleidig@icloud.com' : null,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.email(),
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                FormBuilderTextField(
+                  name: 'subject_dob',
+                  initialValue: kDebugMode ? '01/01/2000' : null,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(8),
+                    DateFormatInputFormatter(DateFormat('dd/MM/yyyy')),
+                  ],
+                  valueTransformer: (value) {
+                    if (value == null) {
+                      return null;
+                    }
+                    // Parse the date, set to start of the day, and convert to UTC
+                    DateTime localDate = DateFormat('dd/MM/yyyy').parse(value);
+                    DateTime localMidnight = DateTime.utc(
+                        localDate.year, localDate.month, localDate.day);
+
+                    DateTime utcDate = localMidnight.toUtc();
+                    return utcDate;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Date of Birth',
+                    hintText: 'dd/mm/yyyy',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                const Text("Recipient"),
+                const SizedBox(height: 16),
+                FormBuilderTextField(
+                  name: 'recipient_email',
+                  initialValue: kDebugMode ? 'georgeleidig@icloud.com' : null,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.email(),
+                  ]),
+                ),
+                const SizedBox(height: 32),
+                const Text("Request"),
+                const SizedBox(height: 16),
+                FormBuilderRadioGroup(
+                  name: 'form',
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  orientation: OptionsOrientation.vertical,
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  options: b2bForms.map((form) {
+                    return FormBuilderFieldOption(
+                      value: form.id,
+                      child: Text(form.name),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 32),
+                FloatingActionButton.extended(
+                  onPressed: () async {
+                    if (_formKey.currentState!.saveAndValidate()) {
+                      setState(() {
+                        isSaving = true;
+                      });
+
+                      // Simulate a delay to show the loading indicator
+                      await Future.delayed(const Duration(seconds: 1));
+
+                      BoomarangRequest request = BoomarangRequest(
+                        id: id,
+                        subjectFirstName: _formKey.currentState!
+                            .fields['subject_first_name']!.value as String,
+                        subjectLastName: _formKey.currentState!
+                            .fields['subject_last_name']!.value as String,
+                        subjectEmail: _formKey.currentState!
+                            .fields['subject_email']!.value as String,
+                        subjectDOB: DateFormat('dd/MM/yyyy').tryParse(
+                            _formKey.currentState!.fields['subject_dob']
+                                    ?.value ??
+                                "",
+                            true),
+                        senderEmail: auth.currentUser!.email!,
+                        recipientEmail: _formKey.currentState!
+                            .fields['recipient_email']!.value as String,
+                        dateCreated: DateTime.now(),
+                        form: b2bForms.firstWhere((element) =>
+                            element.id ==
+                            _formKey.currentState!.fields['form']!.value),
+                      );
+                      //add isDemo: true to the request map
+
+                      Map<String, dynamic> requestMap = request.toMap();
+                      requestMap['isDemo'] = true;
+
+                      firestore.collection('requests').doc(id).set(requestMap);
+
+                      await Future.delayed(const Duration(milliseconds: 300));
+
+                      setState(() {
+                        isSaving = false;
+                      });
+
+                      if (!context.mounted) return;
+                      context.read<TabIndexProvider>().setTabIndex(2);
+                    }
+                  },
+                  label: isSaving
+                      ? const CircularProgressIndicator.adaptive()
+                      : const Text("Submit"),
+                ),
+                const SizedBox(
+                  height: 100,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
