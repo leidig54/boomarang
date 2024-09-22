4fb0d0a306d7893ac6e9b4827850c89dbdda5253
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-22 17:09:27 +0000

    feat(admin/create_demo.js): Add veterinary insurance claim form to demo environment
    - Removed old b2b forms for background checks, medical records, academic transcripts, rental histories, and client financial records.
    - Added a new veterinary insurance claim form with fields for symptom start date, treatment date, condition type, diagnosis, treatment details, and cost.
    - Updated createDemoEnvironment function to randomly select the new form.

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -2,9 +2,9 @@ import 'package:boomarang/data/templates.dart';
 import 'package:boomarang/main.dart';
 import 'package:boomarang/providers/tab_provider.dart';
 import 'package:boomarang_shared/dob_formatter.dart';
+import 'package:boomarang_shared/models/boomarang_element.dart';
 import 'package:boomarang_shared/models/consent_form.dart';
 import 'package:boomarang_shared/models/request.dart';
-import 'package:boomarang_shared/models/request_form.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,10 +24,12 @@ class CreateNewRequest extends StatefulWidget {
 }
 
 class _CreateNewRequestState extends State<CreateNewRequest> {
+  List<FormElement> elements = [];
+  //formbuilder key
   final _formKey = GlobalKey<FormBuilderState>();
+  List<String> forms = ['Form 1', 'Form 2', 'Form 3', 'Form 4'];
   bool isSaving = false;
   late String id;
-  RequestForm? selectedForm;
 
   @override
   void initState() {
@@ -37,14 +39,14 @@ class _CreateNewRequestState extends State<CreateNewRequest> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          constraints: const BoxConstraints(
-            maxWidth: 600,
-          ),
-          child: FormBuilder(
-            key: _formKey,
+    return FormBuilder(
+      key: _formKey,
+      child: Row(
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxWidth: 600,
+            ),
             child: ListView(
               padding: const EdgeInsets.all(32),
               children: [
@@ -144,19 +146,16 @@ class _CreateNewRequestState extends State<CreateNewRequest> {
                   ),
                   orientation: OptionsOrientation.vertical,
                   onChanged: (value) {
-                    setState(() {
-                      selectedForm = forms.firstWhere(
-                          (element) => element.id == value.toString());
-                    });
+                    setState(() {});
                   },
-                  options: forms.map((form) {
+                  options: b2bForms.map((form) {
                     return FormBuilderFieldOption(
                       value: form.id,
                       child: Text(form.name),
                     );
                   }).toList(),
                 ),
-                const SizedBox(height: 16),
+                const SizedBox(height: 32),
                 FloatingActionButton.extended(
                   onPressed: () async {
                     if (_formKey.currentState!.saveAndValidate()) {
@@ -171,22 +170,22 @@ class _CreateNewRequestState extends State<CreateNewRequest> {
                         id: 'general_consent',
                         title: 'General Data Consent Form',
                         content: '''
-                ## General Data Consent Form
-                
-                By providing your consent, you allow us to request and share your data for the purpose outlined in the data request.
-                
-                The data requested may include sensitive information such as:
-                
-                - Medical records
-                - Financial details
-                - Employment history
-                - Other personal data
-                
-                We assure you that your data will be handled securely and in compliance with relevant data protection laws (e.g., GDPR). 
-                You have the right to withdraw your consent at any time.
-                
-                By clicking **Agree**, you confirm that you understand the nature of the request and consent to the transfer of your data.
-              ''',
+    ## General Data Consent Form
+    
+    By providing your consent, you allow us to request and share your data for the purpose outlined in the data request.
+    
+    The data requested may include sensitive information such as:
+    
+    - Medical records
+    - Financial details
+    - Employment history
+    - Other personal data
+    
+    We assure you that your data will be handled securely and in compliance with relevant data protection laws (e.g., GDPR). 
+    You have the right to withdraw your consent at any time.
+    
+    By clicking **Agree**, you confirm that you understand the nature of the request and consent to the transfer of your data.
+  ''',
                       );
 
                       BoomarangRequest request = BoomarangRequest(
@@ -206,7 +205,7 @@ class _CreateNewRequestState extends State<CreateNewRequest> {
                         recipientEmail: _formKey.currentState!
                             .fields['recipient_email']!.value as String,
                         dateCreated: DateTime.now(),
-                        form: forms.firstWhere((element) =>
+                        form: b2bForms.firstWhere((element) =>
                             element.id ==
                             _formKey.currentState!.fields['form']!.value),
                         consentForm: consentForm,
@@ -238,8 +237,8 @@ class _CreateNewRequestState extends State<CreateNewRequest> {
               ],
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
