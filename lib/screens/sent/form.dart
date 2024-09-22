1738b691429f900edbdc737af6490b269e245779
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-22 17:09:27 +0000

    feat(admin/create_demo): Add veterinary insurance claim form
    - Replaced existing B2B forms with a veterinary insurance claim form.
    - Updated the request form handling to include new veterinary form elements.

--- lib/screens/sent/form.dart
+++ lib/screens/sent/form.dart
@@ -22,7 +22,6 @@ class _SentRequestFormState extends State<SentRequestForm> {
   @override
   Widget build(BuildContext context) {
     BoomarangRequest request = widget.selectedRequest;
-    print(request.response);
 
     return FormBuilder(
       key: formKey,
@@ -43,7 +42,6 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderTextField(
-                      key: Key(element.id),
                       name: element.id,
                       readOnly: true,
                       enabled: false,
@@ -62,7 +60,6 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderCheckbox(
-                      key: Key(element.id),
                       name: element.id,
                       enabled: false,
                       onChanged: null,
@@ -82,7 +79,6 @@ class _SentRequestFormState extends State<SentRequestForm> {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderRadioGroup(
-                      key: Key(element.id),
                       name: element.id,
                       enabled: false,
                       initialValue: initialValue,
