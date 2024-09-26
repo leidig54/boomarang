85a36cfbc44b0a4241dcfa0b57fd0dc733c2ad5f
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-26 13:36:51 +0000

    feat(organisation): Implement organisation management feature
    - Created a new OrganisationProvider for handling organisation data.
    - Added functionality to create a demo organisation and save it to Firestore.
    - Updated OrganisationScreen to retrieve and display organisation details.
    - Renamed the settings screen and adjusted imports for clarity.

--- lib/providers/tab_provider.dart
+++ lib/providers/tab_provider.dart
@@ -1,7 +1,7 @@
 import 'package:boomarang/screens/dashboard.dart';
 import 'package:boomarang/screens/inbox/inbox.dart';
 import 'package:boomarang/screens/sent/sent.dart';
-import 'package:boomarang/screens/shared/settings.dart';
+import 'package:boomarang/screens/settings.dart';
 import 'package:flutter/material.dart';
 
 class TabIndexProvider extends ChangeNotifier {
