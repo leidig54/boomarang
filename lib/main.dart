059b5d478f1a6a075d97aa8801aeb3c9403097da
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-26 21:35:38 +0100

    stuff

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
-//TODO: Provide request templates
-
 bool useEmulators = true;
 
 FirebaseAuth auth = FirebaseAuth.instance;
