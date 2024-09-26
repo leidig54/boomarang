3d4e6edc8fd298eb74492cd977500b26293e0d02
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-26 20:35:38 +0000

    feat(organisation): Implement user management UI
    - Removed TODO comments regarding request templates and user permissions
    - Added a section for user management to display users and their roles
    - Implemented FutureBuilder to fetch user details from Firestore
    - Added a dropdown for changing user roles between Admin and Member.

---
+++ lib/screens/nav/home.dart
@@ -0,0 +1,95 @@
+import 'package:boomarang/providers/tab_provider.dart';
+import 'package:boomarang/screens/nav/navigation_bottom_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        NavigationRail(
+          leading: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Transform.flip(
+                  flipX: true,
+                  child: const FlutterLogo(size: 100),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'Boomarang',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+            //Boomerang
+          ),
+          destinations: [
+            NavigationRailDestination(
+              icon: const Icon(Icons.dashboard),
+              label: Text(
+                'Dashboard',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.inbox),
+              label: Text(
+                'Inbox',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.outbox),
+              label: Text(
+                'Sent',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            NavigationRailDestination(
+              icon: const Icon(Icons.person),
+              label: Text(
+                'Admin',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ],
+          trailing: const NavigationRailTrailingWidget(),
+          selectedIndex: context.watch<TabIndexProvider>().tabIndex,
+          onDestinationSelected: (int index) {
+            context.read<TabIndexProvider>().setTabIndex(index);
+          },
+          extended: true,
+        ),
+        const VerticalDivider(
+          thickness: 3,
+          width: 3,
+        ),
+        Expanded(
+          child: Scaffold(
+            body: context.watch<TabIndexProvider>().screen,
+          ),
+        ),
+      ],
+    );
+  }
+}
