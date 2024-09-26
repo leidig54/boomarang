c0ffbadced783c4667b4b043cca8fcdbabc61837
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-26 07:28:22 +0000

    feat(forms): Change text input types to date for symptom and treatment dates
    - Updated 'symptom_start_date' and 'consultation_date' fields to use date input type in create_demo.js
    - Modified form handling for date fields in inbox and sent forms
    - Refactored form element model to use new 'form_element.dart'

--- lib/screens/sent/form.dart
+++ lib/screens/sent/form.dart
@@ -1,7 +1,9 @@
-import 'package:boomarang_shared/models/boomarang_element.dart';
+import 'package:boomarang_shared/models/form_element.dart';
 import 'package:boomarang_shared/models/request.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
 
 class SentRequestForm extends StatefulWidget {
   const SentRequestForm({
@@ -28,6 +30,18 @@ class _SentRequestFormState extends State<SentRequestForm> {
       child: ListView(
         padding: const EdgeInsets.all(32.0),
         children: [
+          Text(request.form.name,
+              style: Theme.of(context).textTheme.headlineMedium),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            request.form.description,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
           if (request.form.elements.isNotEmpty == true)
             ...List.generate(
               request.form.elements.length,
@@ -35,35 +49,30 @@ class _SentRequestFormState extends State<SentRequestForm> {
                 FormElement element = request.form.elements[index];
 
                 if (element.type == "text") {
-                  dynamic initialValue;
-                  if (request.response?[element.id] != null) {
-                    initialValue = request.response?[element.id];
-                  }
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderTextField(
                       name: element.id,
                       readOnly: true,
                       enabled: false,
-                      initialValue: initialValue,
+                      initialValue: request.response?[element.id],
+                      minLines: element.minLines,
+                      maxLines: element.maxLines,
                       decoration: InputDecoration(
                         labelText: element.labelText,
                         border: const OutlineInputBorder(),
+                        alignLabelWithHint: true,
                       ),
                     ),
                   );
                 } else if (element.type == "checkbox") {
-                  dynamic initialValue;
-                  if (request.response?[element.id] != null) {
-                    initialValue = request.response?[element.id];
-                  }
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderCheckbox(
                       name: element.id,
                       enabled: false,
                       onChanged: null,
-                      initialValue: initialValue,
+                      initialValue: request.response?[element.id],
                       controlAffinity: ListTileControlAffinity.trailing,
                       title: Text(element.labelText!),
                       decoration: const InputDecoration(
@@ -72,16 +81,12 @@ class _SentRequestFormState extends State<SentRequestForm> {
                     ),
                   );
                 } else if (element.type == 'radio') {
-                  dynamic initialValue;
-                  if (request.response?[element.id] != null) {
-                    initialValue = request.response?[element.id];
-                  }
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 32.0),
                     child: FormBuilderRadioGroup(
                       name: element.id,
                       enabled: false,
-                      initialValue: initialValue,
+                      initialValue: request.response?[element.id],
                       decoration: InputDecoration(
                         labelText: element.labelText,
                         border: const OutlineInputBorder(),
@@ -100,6 +105,35 @@ class _SentRequestFormState extends State<SentRequestForm> {
                                 ),
                               ))
                           .toList(),
+                    ),
+                  );
+                } else if (element.type == "statement") {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      element.text!,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Colors.grey,
+                          ),
+                    ),
+                  );
+                } else if (element.type == "date") {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 32.0),
+                    child: FormBuilderDateTimePicker(
+                      name: element.id,
+                      enabled: false,
+                      initialValue: request.response?[element.id] != null
+                          ? (request.response?[element.id] as Timestamp)
+                              .toDate()
+                          : null,
+                      inputType: InputType.date,
+                      format: DateFormat.yMMMMd(),
+                      initialEntryMode: DatePickerEntryMode.calendarOnly,
+                      decoration: InputDecoration(
+                        labelText: element.labelText,
+                        border: const OutlineInputBorder(),
+                      ),
                     ),
                   );
                 } else {
