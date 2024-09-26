4afa641da9c3b8e25345efdac7a54e0d3dea19fd
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-26 07:28:22 +0000

    feat(demographics): Update form element types and functionality
    - Changed input types from 'text' to 'date' for date fields.
    - Refactored form element handling to accommodate min and max lines for text inputs.
    - Updated UI elements to reflect improved user experience.
    - Added handling for date inputs in various screens.

--- lib/screens/nav/home.dart
+++ lib/screens/nav/home.dart
@@ -30,7 +30,10 @@ class _HomeState extends State<Home> {
                   const SizedBox(
                     height: 40,
                   ),
-                  const FlutterLogo(size: 100),
+                  Transform.flip(
+                    flipX: true,
+                    child: const FlutterLogo(size: 100),
+                  ),
                   const SizedBox(
                     height: 40,
                   ),
