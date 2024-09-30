1e12c6a192c6435243ce55c486efbddf00a6b679
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-30 13:59:17 +0000

    stuff

--- lib/screens/sent/form.dart
+++ lib/screens/sent/form.dart
@@ -31,7 +31,7 @@ class _SentRequestFormState extends State<SentRequestForm> {
         padding: const EdgeInsets.all(32.0),
         children: [
           Text(request.form.name,
-              style: Theme.of(context).textTheme.headlineMedium),
+              style: Theme.of(context).textTheme.headlineSmall),
           const SizedBox(
             height: 4,
           ),
