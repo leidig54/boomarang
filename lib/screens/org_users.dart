bff3aac2322789b69cd3f619e071d4bb6dfe674c
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-09-30 18:36:51 +0000

    org stuff

--- lib/screens/org_users.dart
+++ lib/screens/org_users.dart
@@ -1,8 +1,8 @@
 import 'package:boomarang/main.dart';
 import 'package:boomarang/providers/organisation_provider.dart';
+import 'package:boomarang/providers/user_provider.dart';
 import 'package:boomarang_shared/models/organisation.dart';
 import 'package:boomarang_shared/models/user.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,7 +26,8 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
       );
     }
 
-    bool isAdmin = organisation.admins.contains(auth.currentUser?.uid);
+    bool isAdmin =
+        context.watch<UserProvider>().user?.organisationRole == 'admin';
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -35,7 +36,7 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
           padding: const EdgeInsets.all(16.0),
           child: Container(
             constraints: const BoxConstraints(
-              maxWidth: 600,
+              maxWidth: 800,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -47,7 +48,7 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
                 const SizedBox(height: 32),
                 ListView.builder(
                   itemBuilder: (context, index) {
-                    String uid = organisation.members[index];
+                    String uid = organisation.users[index];
                     return FutureBuilder(
                         future: firestore.collection('users').doc(uid).get(),
                         builder: (context, snapshot) {
@@ -57,8 +58,7 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
                           if (snapshot.hasData && snapshot.data!.exists) {
                             user = BoomarangUser.fromMap(
                                 snapshot.data!.data() as Map<String, dynamic>);
-                            userIsAdmin =
-                                organisation.admins.contains(snapshot.data!.id);
+                            userIsAdmin = user.organisationRole == 'admin';
                           }
 
                           if (user == null || userIsAdmin == null) {
@@ -70,25 +70,31 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
                             children: [
                               Expanded(
                                 flex: 2,
-                                child:
-                                    Text('${user.firstName} ${user.lastName}'),
+                                child: Text(
+                                    '${user.firstName} ${user.lastName}',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium),
                               ),
                               Expanded(
-                                flex: 2,
-                                child: Text('${user.email}'),
+                                flex: 3,
+                                child: Text('${user.email}',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium),
                               ),
                               Expanded(
                                 child: PermissionsDropdown(
-                                    userIsAdmin: userIsAdmin,
-                                    isAdmin: isAdmin,
-                                    organisation: organisation,
-                                    user: user),
+                                  isAdmin: isAdmin,
+                                  organisation: organisation,
+                                  user: user,
+                                ),
                               )
                             ],
                           );
                         });
                   },
-                  itemCount: organisation.members.length,
+                  itemCount: organisation.users.length,
                   shrinkWrap: true,
                 ),
               ],
@@ -103,13 +109,11 @@ class _OrganisationUsersScreenState extends State<OrganisationUsersScreen> {
 class PermissionsDropdown extends StatefulWidget {
   const PermissionsDropdown({
     super.key,
-    required this.userIsAdmin,
     required this.isAdmin,
     required this.organisation,
     required this.user,
   });
 
-  final bool userIsAdmin;
   final bool isAdmin;
   final Organisation organisation;
   final BoomarangUser user;
@@ -123,77 +127,64 @@ class _PermissionsDropdownState extends State<PermissionsDropdown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-        value: widget.userIsAdmin ? 'admin' : 'member',
-        disabledHint: const CircularProgressIndicator.adaptive(),
-        isExpanded: true,
-        items: isUpdatingPermissions
-            ? null
-            : [
-                const DropdownMenuItem(
-                  value: 'admin',
-                  child: Text('Admin'),
-                ),
-                const DropdownMenuItem(
-                  value: 'member',
-                  child: Text('Member'),
-                ),
-              ],
-        onChanged: !widget.isAdmin
-            ? null
-            : (value) async {
-                //if the value is the same, don't do anything
-                if ((value == 'admin' && widget.userIsAdmin) ||
-                    (value == 'member' && !widget.userIsAdmin)) {
-                  return;
-                }
-                setState(() {
-                  isUpdatingPermissions = true;
-                });
-                await Future.delayed(const Duration(seconds: 1));
-                if (value == 'admin') {
-                  await firestore
-                      .collection('organisations')
-                      .doc(widget.organisation.id)
-                      .update({
-                    'admins': FieldValue.arrayUnion([widget.user.id])
-                  });
-                } else {
-                  //if is the last admin, show an alert dialog and don't remove
-                  if (widget.organisation.admins.length == 1) {
-                    if (!context.mounted) return;
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: const Text('Cannot remove last admin'),
-                          content: const Text(
-                              'You cannot remove the last admin from an organisation. Please add another admin before removing this user.'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    await firestore
-                        .collection('organisations')
-                        .doc(widget.organisation.id)
-                        .update({
-                      'admins': FieldValue.arrayRemove([widget.user.id])
+    return StreamBuilder(
+        stream: firestore.collection('users').doc(widget.user.id).snapshots(),
+        builder: (context, snapshot) {
+          BoomarangUser? user;
+          if (snapshot.hasData && snapshot.data!.exists) {
+            user = BoomarangUser.fromMap(
+                snapshot.data!.data() as Map<String, dynamic>);
+          }
+
+          return DropdownButton(
+              value: user?.organisationRole,
+              disabledHint: const CircularProgressIndicator.adaptive(),
+              isExpanded: true,
+              underline: const SizedBox(),
+              items: isUpdatingPermissions
+                  ? null
+                  : [
+                      const DropdownMenuItem(
+                        value: 'admin',
+                        child: Text('Admin'),
+                      ),
+                      const DropdownMenuItem(
+                        value: 'user',
+                        child: Text('User'),
+                      ),
+                    ],
+              onChanged: !widget.isAdmin
+                  ? null
+                  : (value) async {
+                      //if the value is the same, don't do anything
+                      if (value == user?.organisationRole) {
+                        return;
+                      }
+                      setState(() {
+                        isUpdatingPermissions = true;
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
+
+                      await functions
+                          .httpsCallable('updateOrganisationRole')
+                          .call({
+                        'id': user?.id,
+                        'organisationId': widget.organisation.id,
+                        'role': value,
+                      }).then((_) {
+                        if (mounted) {
+                          setState(() {
+                            isUpdatingPermissions = false;
+                          });
+                        }
+                      }).catchError((error) {
+                        if (mounted) {
+                          setState(() {
+                            isUpdatingPermissions = false;
+                          });
+                        }
+                      });
                     });
-                  }
-                }
-                if (mounted) {
-                  setState(() {
-                    isUpdatingPermissions = false;
-                  });
-                }
-              });
+        });
   }
 }
