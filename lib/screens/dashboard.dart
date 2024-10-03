e4be9168038f8ac1bc720d5cb4029b4c1b7fa47a
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-10-03 19:46:24 +0000

    feat(organisation): Implement create and join organisation features
    - Added CreateOrganisation screen for users to create a new organisation.
    - Added joinOrganisation function to allow users to join an existing organisation using an invite code.
    - Implemented functionality to send invites to users and manage pending invites in the organisation.
    - Updated email sending mechanism to a shared function to avoid repetition.
    - Refactored the dashboard and onboarding screens to accommodate for the new organisational flow.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,6 +1,6 @@
-import 'package:boomarang/providers/tab_provider.dart';
 import 'package:boomarang/providers/user_provider.dart';
 import 'package:boomarang/screens/create.dart';
+import 'package:boomarang/screens/profile.dart';
 import 'package:boomarang_shared/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,12 +13,12 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  String selected = 'create';
+
   @override
   Widget build(BuildContext context) {
     BoomarangUser? user = context.watch<UserProvider>().user;
     bool profileIsComplete = user?.profileIsComplete ?? false;
-
-    String selected = 'create';
 
     return Scaffold(
         body: Column(
@@ -47,7 +47,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
           child: Row(
             children: [
               Expanded(
-                flex: 2,
+                flex: 3,
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
@@ -64,13 +64,21 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       ListTile(
                         title: const Text('Complete Profile'),
                         enabled: !profileIsComplete,
-                        subtitle: const Text(
-                            'Complete your profile to share with others when requesting data.'),
+                        trailing: const Icon(Icons.chevron_right),
+                        subtitle: const Text('Provide your personal details.'),
+                        tileColor: selected == 'profile'
+                            ? Theme.of(context).highlightColor
+                            : null,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                         leading: profileIsComplete
                             ? const Icon(Icons.check_box)
                             : const Icon(Icons.check_box_outline_blank),
                         onTap: () {
-                          context.read<TabIndexProvider>().setTabIndex(3);
+                          setState(() {
+                            selected = 'profile';
+                          });
                         },
                       ),
 
@@ -94,7 +102,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
                         tileColor: selected == 'create'
                             ? Theme.of(context).highlightColor
                             : null,
-                        onTap: () {},
+                        onTap: () {
+                          setState(() {
+                            selected = 'create';
+                          });
+                        },
                       ),
                       Padding(
                         padding: const EdgeInsets.only(
@@ -139,9 +151,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 width: 1,
                 thickness: 0.5,
               ),
-              const Expanded(
-                flex: 3,
-                child: CreateNewRequest(),
+              Expanded(
+                flex: 7,
+                child: Builder(builder: (context) {
+                  if (selected == 'profile') {
+                    return const BoomarangProfileScreen();
+                  } else {
+                    return const CreateNewRequest();
+                  }
+                }),
               )
             ],
           ),
