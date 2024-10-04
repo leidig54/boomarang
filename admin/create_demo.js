a8544b1258d26f41e4ee3929c93a5bef18c664fe
George Leidig <56688570+leidig54@users.noreply.github.com>
2024-10-04 10:10:23 +0000

    chore: update greeting message to improve clarity

--- admin/create_demo.js
+++ admin/create_demo.js
@@ -72,6 +72,15 @@ async function createDemoEnvironment() {
     requests.docs.map((request) => requestCollection.doc(request.id).delete())
   );
 
+  // For clearing the organisations collection
+  const organisationsCollection = db.collection("organisations");
+  const organisations = await organisationsCollection.get();
+  await Promise.all(
+    organisations.docs.map((organisation) =>
+      organisationsCollection.doc(organisation.id).delete()
+    )
+  );
+
   if (withUser) {
     const auth = admin.auth();
     //clear all users from firebase auth
@@ -123,7 +132,6 @@ async function createDemoEnvironment() {
     users: ["1", "2", "3", "4", "5", "6"],
   };
 
-  const organisationsCollection = db.collection("organisations");
   await organisationsCollection.doc(organisation.id).set(organisation);
 
   const createRequest = async () => {
